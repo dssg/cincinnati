a2c3a6d3721570093aad09f9bac2124c79df1928
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-12-11 14:50:27 -0600

    taxes etl done

--- etl/taxes/taxes.sh
+++ etl/taxes/taxes.sh
@@ -1,37 +1,19 @@
 #!/usr/bin/env bash
-TAXES_FOLDER="$DATA_FOLDER/etl/taxes"
+
+#Parse raw taxes data files and generate CSVs
+bash "$ROOT_FOLDER/etl/taxes/parse_taxes.sh"
+
+#Upload CSVs to the database
+
+#Get tmp folder for taxes etl
 TMP_FOLDER="$DATA_FOLDER/etl/taxes/tmp"
-
-#Read variables from config file
-DB_HOST=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.host)
-DB_USER=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.user)
-DB_NAME=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.database)
-
-#Parse files from 2007 to 2014, first parameter is filename, second is year
-#the python script automatically performs operations in the corresponding
-#data folder and outputs the result in the corresponding tmp folder
-python "$ROOT_FOLDER/etl/taxes/parse_text_tax_file.py" taxinfo2007.txt 2007
-python "$ROOT_FOLDER/etl/taxes/parse_text_tax_file.py" taxinfo2008.txt 2008
-python "$ROOT_FOLDER/etl/taxes/parse_text_tax_file.py" taxinfo2009.txt 2009
-python "$ROOT_FOLDER/etl/taxes/parse_text_tax_file.py" taxinfo2010.txt 2010
-python "$ROOT_FOLDER/etl/taxes/parse_text_tax_file.py" taxinfo2011.txt 2011
-python "$ROOT_FOLDER/etl/taxes/parse_text_tax_file.py" taxinfo2012.txt 2012
-python "$ROOT_FOLDER/etl/taxes/parse_text_tax_file.py" taxinfo2013.txt 2013
-python "$ROOT_FOLDER/etl/taxes/parse_text_tax_file.py" taxinfo2014.txt 2014
-
-#File from 2015 is a CSV, we only need to append the header
-python "$ROOT_FOLDER/etl/taxes/add_header_csv_tax_file.py" Tax_Information2015.CSV 2015
-
-
-#####
-
-#Use csvsql to create a SQL script with the CREATE TABLE statement
-csvsql -i postgresql --tables taxes_2007 --db-schema public -d ',' "$TMP_FOLDER/taxes_2007.csv" > "$TMP_FOLDER/taxes_2007.sql"
-
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS crime;"  
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$TMP_FOLDER/crime.sql"  
-
-#Import the data into the new create table
-cat "$TMP_FOLDER/2004-2014_cleaned.csv" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "\COPY public.crime FROM STDIN  WITH CSV HEADER DELIMITER ';';"
-
-echo 'Done creating crime table!'
+#Upload taxes to the database
+bash "$ROOT_FOLDER/etl/taxes/upload_tax.sh" $TMP_FOLDER taxes_2007.csv taxes_2007
+bash "$ROOT_FOLDER/etl/taxes/upload_tax.sh" $TMP_FOLDER taxes_2008.csv taxes_2008
+bash "$ROOT_FOLDER/etl/taxes/upload_tax.sh" $TMP_FOLDER taxes_2009.csv taxes_2009
+bash "$ROOT_FOLDER/etl/taxes/upload_tax.sh" $TMP_FOLDER taxes_2010.csv taxes_2010
+bash "$ROOT_FOLDER/etl/taxes/upload_tax.sh" $TMP_FOLDER taxes_2011.csv taxes_2011
+bash "$ROOT_FOLDER/etl/taxes/upload_tax.sh" $TMP_FOLDER taxes_2012.csv taxes_2012
+bash "$ROOT_FOLDER/etl/taxes/upload_tax.sh" $TMP_FOLDER taxes_2013.csv taxes_2013
+bash "$ROOT_FOLDER/etl/taxes/upload_tax.sh" $TMP_FOLDER taxes_2014.csv taxes_2014
+bash "$ROOT_FOLDER/etl/taxes/upload_tax.sh" $TMP_FOLDER taxes_2015.csv taxes_2015
--- etl/taxes/upload_taxes.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-TMP_FOLDER="$DATA_FOLDER/etl/taxes/tmp"
-
-bash "$ROOT_FOLDER/etl/taxes/upload_tax.sh" $TMP_FOLDER taxes_2007.csv taxes_2007
