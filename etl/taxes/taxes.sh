974102317a63f0dc74358fe24354b5ee9dcc0b5f
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-12-10 19:58:49 -0600

    renaming taxes etl

--- etl/taxes/taxes.sh
+++ etl/taxes/taxes.sh
@@ -7,12 +7,9 @@ DB_HOST=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.host)
 DB_USER=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.user)
 DB_NAME=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.database)
 
-#mkdir if not exists
-mkdir -p $TMP_FOLDER
-
-echo "Converting data from $TAXES_FOLDER"
+echo "Loading data from $TAXES_FOLDER"
 
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS taxes_2015;"
 
 #Load taxes file and create sql table
-python "$ROOT_FOLDER/etl/taxes/Extract2015Taxes.py"
+python "$ROOT_FOLDER/etl/taxes/extract_taxes_2015.py.py"
