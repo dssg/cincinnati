7156e5c7fee148fdb1b1ed042eed9cd0d2cc9b7d
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-12-14 22:50:50 +0000

    uploading directly from csvsql

--- etl/taxes/upload_tax.sh
+++ etl/taxes/upload_tax.sh
@@ -10,17 +10,20 @@ TABLE_NAME=$3
 DB_HOST=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.host)
 DB_USER=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.user)
 DB_NAME=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.database)
+DB_PASSWORD=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.password)
 
 #Use csvsql to create a SQL script with the CREATE TABLE statement
 echo "Generating CREATE TABLE statement from csv file..."
-#For now, add the no inference parameter to avoid trouble with certain numeric columns that have commas
-csvsql --no-inference -i postgresql --tables $TABLE_NAME --db-schema public -d ',' "$FOLDER/$CSV_FILENAME" > "$FOLDER/$TABLE_NAME.sql"
+
+csvsql --db "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:5432/$DB_NAME"  --insert  --tables $TABLE_NAME --db-schema public -d ',' "$FOLDER/$CSV_FILENAME" > "$FOLDER/$TABLE_NAME.sql"
 #Drop tables if they already exist
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS $TABLE_NAME;"  
+#psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS $TABLE_NAME;"  
+
 #Run the CREATE TABLE statements on the database
 echo "Running CREATE TABLE statement..."
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$FOLDER/$TABLE_NAME.sql"
+#psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$FOLDER/$TABLE_NAME.sql"
+
 #Import the data into the tables
 echo "Uploading data..."
-cat "$FOLDER/$CSV_FILENAME" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "\COPY public.$TABLE_NAME FROM STDIN  WITH CSV HEADER DELIMITER ',';"
+#cat "$FOLDER/$CSV_FILENAME" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "\COPY public.$TABLE_NAME FROM STDIN  WITH CSV HEADER DELIMITER ',';"
 echo "Done creating $TABLE_NAME table!"
