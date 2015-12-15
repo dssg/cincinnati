49202c00263d9dd709b8dd0a1ba524aefbfcec52
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-12-15 13:31:41 -0600

    script to drop raw vendor tables

--- etl/inspections/inspections.sh
+++ etl/inspections/inspections.sh
@@ -6,16 +6,21 @@ DB_HOST=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.host)
 DB_USER=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.user)
 DB_NAME=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.database)
 
+#Creates table on default schema (generally this is the public schema)
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS bld_info;"  
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/auditor_building_info.tables.sql"
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/auditor_building_info.data.sql"
 
 
 #Creates tables on inspections schema:
 #t_dssg_csv_enf_viol and t_dssg_csv_enf_insp
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS inspections.t_dssg_csv_enf_viol;"  
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS inspections.t_dssg_csv_enf_insp;"  
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/cag_code_violations.tables.sql"
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/cag_code_violations.data.sql"
 
 #Creates tables on inspections_raw schema
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/drop_raw_vendor_tables.sql"
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/raw_vendor_tables.tables.sql"
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/raw_vendor_tables.data.sql"
 
