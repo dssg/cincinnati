17f40d03fd8c5c94c82037946721269324fe3499
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-12-16 12:14:42 -0600

    fixes typo in schema name and moves parcel id script to inspections etl folder

--- etl/inspections/inspections.sh
+++ etl/inspections/inspections.sh
@@ -7,12 +7,15 @@ DB_HOST=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.host)
 DB_USER=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.user)
 DB_NAME=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.database)
 
+#Part 1: Building info
 #Creates table on default schema (generally this is the public schema)
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS bld_info;"  
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/auditor_building_info.tables.sql"
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/auditor_building_info.data.sql"
+#Run this code to fix parcel_id
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$ROOT_FOLDER/etl/inspections/fix_parcel_id.sql"
 
-
+#Part 2: Code violations
 #Creates tables on inspections schema:
 #t_dssg_csv_enf_viol and t_dssg_csv_enf_insp
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS inspections.t_dssg_csv_enf_viol;"  
@@ -20,6 +23,7 @@ psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS inspections.t_
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/cag_code_violations.tables.sql"
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/cag_code_violations.data.sql"
 
+#Part 3: Vendor tables
 #Creates tables on inspections_raw schema
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_CODE_FOLDER/drop_raw_vendor_tables.sql"
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_DATA_FOLDER/raw_vendor_tables.tables.sql"
