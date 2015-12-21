30fb094d7f65933360204ca26c9cb7619cd07b49
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-12-21 14:03:06 -0600

    changes merge save path

--- etl/ner/merge.py
+++ etl/ner/merge.py
@@ -1,5 +1,19 @@
 import pandas as pd
+from python_ds_tools import data_folder
 
+#Move current directory do all I/O operations take place in the corresponding
+#Data folder
+data_folder = data_folder.for_file(__file__)
+
+#Create tmp file if it does not exist
+if not os.path.exists('tmp'):
+    print 'Creating tmp folder in %s' % os.getcwd()
+    os.makedirs('tmp')
+
+#Move to tmp folder
+os.chdir(os.path.join(data_folder, 'tmp'))
+
+print 'Changing working dir to: %s' % os.getcwd()
 
 def read_for_year(year):
     df = pd.read_csv("owners_{}_resolved.csv".format(year))
