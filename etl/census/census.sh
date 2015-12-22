1161c2040406b9fac4cb3a1c20647eb42411cee3
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-12-22 16:14:56 -0600

    adds code for census tracts

--- etl/census/census.sh
+++ etl/census/census.sh
@@ -17,13 +17,23 @@ mkdir -p $TMP_FOLDER
 
 #BLOCK GROUPS
 #0. Get the files
-#wget ftp://ftp2.census.gov/geo/pvs/tiger2010st/39_Ohio/39/tl_2010_39_bg10.zip
+wget "ftp://ftp2.census.gov/geo/pvs/tiger2010st/39_Ohio/39/tl_2010_39_bg10.zip" --directory-prefix=$TMP_FOLDER
+unzip "$TMP_FOLDER/tl_2010_39_bg10.zip"
 #1. Convert to pgsql, SRID is 4269, convert to 3735
 shp2pgsql -s 4269:3735 "$TMP_FOLDER/tl_2010_39_bg10.shp" shape_files.census_blocks_groups > "$TMP_FOLDER/census_blocks_groups.sql"
 #2. Drop table if exists
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS shape_files.census_blocks_groups;"  
 #3. Upload to the database
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$TMP_FOLDER/census_blocks_groups.sql"
+
+#BLOCKS
+
+#TRACTS
+wget "ftp://ftp2.census.gov/geo/pvs/tiger2010st/39_Ohio/39/tl_2010_39_tract10.zip" --directory-prefix=$TMP_FOLDER
+unzip "$TMP_FOLDER/tl_2010_39_tract10.zip"
+shp2pgsql -s 4269:3735 "$TMP_FOLDER/tl_2010_39_tract10.shp" shape_files.census_tracts > "$TMP_FOLDER/census_tracts.sql"
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS shape_files.census_tracts;"  
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$TMP_FOLDER/census_tracts.sql"
 
 
 #Download census data - creates table shape_files.census_pop_housing
