8ed163b180fbddc655614d63a89c2940165683fd
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-04 19:40:36 +0000

    comments feature inspections table creation

--- blight_risk_prediction/features/featurebot.py
+++ blight_risk_prediction/features/featurebot.py
@@ -34,6 +34,7 @@ FeatureToGenerate = namedtuple("FeatureToGenerate",
 
 
 features_to_generate = [FeatureToGenerate("tax", tax.make_tax_features)]
+#features_to_generate = [FeatureToGenerate("crime", crime.make_crime_features)]
 
 class SchemaMissing():
     def __init__(self, schema_name):
@@ -124,12 +125,12 @@ def generate_features_for_fake_inspection(inspection_date):
     # this table has two columns: parcel_id, inspection_date
     # inspection_date is the one give as a parameter and is the same
     # for all parcels
-    logging.info("Generating inspections table")
-    inspections = outcome.make_fake_inspections_all_parcels_cincy(
-        inspection_date)
-    inspections.to_sql("parcels_inspections", engine, chunksize=50000,
-                       if_exists='fail', index=False, schema=schema)
-    logging.debug("... table has {} rows".format(len(inspections)))
+    #logging.info("Generating inspections table")
+    #inspections = outcome.make_fake_inspections_all_parcels_cincy(
+    #    inspection_date)
+    #inspections.to_sql("parcels_inspections", engine, chunksize=50000,
+    #                   if_exists='fail', index=False, schema=schema)
+    #logging.debug("... table has {} rows".format(len(inspections)))
 
     # make features and store in database
     for feature in features_to_generate:
