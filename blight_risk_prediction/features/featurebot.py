544a8d6db6e94b95b822ac4c9bf4ffc1cb221a38
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-04 19:45:01 +0000

    comments inspections table creation

--- blight_risk_prediction/features/featurebot.py
+++ blight_risk_prediction/features/featurebot.py
@@ -79,11 +79,11 @@ def generate_features():
     # this table has three columns: parcel_id, inspection_date, viol_outcome
     # inspection_date is the one given as a parameter and
     # is the same for all parcels
-    logging.info("Generating inspections table")
-    inspections = outcome.generate_labels()
-    inspections.to_sql("parcels_inspections", engine, chunksize=50000,
-                       if_exists='fail', index=False, schema=schema)
-    logging.debug("... table has {} rows".format(len(inspections)))
+    #logging.info("Generating inspections table")
+    #inspections = outcome.generate_labels()
+    #inspections.to_sql("parcels_inspections", engine, chunksize=50000,
+    #                   if_exists='fail', index=False, schema=schema)
+    #logging.debug("... table has {} rows".format(len(inspections)))
 
     # make features and store in database
     for feature in features_to_generate:
