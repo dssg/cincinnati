0aca9507c082e65053010de5b22f76d6c1690a9f
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-12 10:59:03 -0600

    changes for blocks groups

--- etl/census/process_census_tables.sql
+++ etl/census/process_census_tables.sql
@@ -31,23 +31,20 @@ CREATE TABLE shape_files.census_blocks AS(
 );
 
 --changes for block groups table
+CREATE TEMPORARY TABLE blocks_groups AS(
+    SELECT  *,
+            tractce10 AS tract,
+            blkgrpce10 AS blkgrp,
+            ST_AREA(geom) AS area,
+            ST_PERIMETER(geom) AS perimeter
+    FROM shape_files.census_blocks_groups
 
+);
+
+DROP TABLE shape_files.census_blocks_groups;
+
+CREATE TABLE shape_files.census_blocks_groups AS(
+    SELECT * FROM blocks_groups
+);
 
 --changes for tracts table
-
---TABLE: shape_files.census_tracts
---Name conversion
---NEW <- OLD
---gid - no change
---objectid <- ?
---tractce10 - no change
---shape_area - all zeros
---shape_len - all zeros
---tract?
---area?
---acres?
---perimeter?
---geom - no change
---The rest of the columns are dropped
-
---Join census blocks with census block groups
