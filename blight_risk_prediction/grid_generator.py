3ea140e32883bade518744ad05183b600cfacfaa
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-14 12:55:51 -0600

    creates new small grid

--- blight_risk_prediction/grid_generator.py
+++ blight_risk_prediction/grid_generator.py
@@ -2,30 +2,9 @@
 #sklearn
 import itertools
 from pydoc import locate
+from grid_values import small_grid
 
-_grid_values = {
-    "sklearn.ensemble.RandomForestClassifier": {
-        'n_estimators': [1,10,100,1000,10000],
-        'max_depth': [1,5,10,20,50,100],
-        'max_features': ['sqrt','log2'],
-        'min_samples_split': [2,5,10]
-    },
-
-    "sklearn.ensemble.AdaBoostClassifier": {
-        'algorithm': ['SAMME', 'SAMME.R'],
-        'n_estimators': [1,10,100,1000,10000]
-    },
-    
-    "sklearn.linear_model.LogisticRegression": {
-        'penalty': ['l1','l2'],
-        'C': [0.00001,0.0001,0.001,0.01,0.1,1,10]
-    },
-    
-    "sklearn.svm.SVC": {
-        'C': [0.00001,0.0001,0.001,0.01,0.1,1,10],
-        'kernel': ['linear']
-    }
-}
+_grid_values = small_grid
 
 def _generate_grid(model_parameters):
     #Iterate over keys and values
