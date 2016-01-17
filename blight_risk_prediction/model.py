545ca36ad6ef21501c24dbf2617decacff3e4831
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-16 22:58:41 -0600

    small changes

--- blight_risk_prediction/model.py
+++ blight_risk_prediction/model.py
@@ -181,20 +181,20 @@ def save_results(pkl_file, config, test, predictions,
         cutoff_at_10, prec_at_10 = evaluation.precision_at_x_percent(test.y, predictions, x_percent=0.1)
         #Sending model will log model name, parameters and datetime
         #Also log other important things by sending named parameters
-	    #logger returns the datetime (UTC) stored in the db to use it as a reference
+	#logger returns the datetime (UTC) stored in the db to use it as a reference
         utc = mongo_logger.log_model(model, features=list(test.feature_names),
                                       feature_importances=list(feature_importances),
                                       config=config, prec_at_1=prec_at_1,
                                       prec_at_10=prec_at_10)
-	   str_local = utc.strftime("%Y-%m-%d_%H:%M:%S")
-	   #Dump test_labels, test_predictions and test_parcels to a csv file
-	   parcel_id = [record[0] for record in test.parcels]
-	   inspection_date = [record[1] for record in test.parcels]
-       dump = pd.DataFrame({'parcel_id': parcel_id,
+	str_local = utc.strftime("%Y-%m-%d_%H:%M:%S")
+	#Dump test_labels, test_predictions and test_parcels to a csv file
+	parcel_id = [record[0] for record in test.parcels]
+	inspection_date = [record[1] for record in test.parcels]
+        dump = pd.DataFrame({'parcel_id': parcel_id,
 			     'inspection_date': inspection_date,
 			     'viol_outcome': test.y,
 			     'prediction': predictions})
-       dump.to_csv(os.path.join(os.environ['OUTPUT_FOLDER'], "predictions", str_local+'.csv'))
+        dump.to_csv(os.path.join(os.environ['OUTPUT_FOLDER'], "predictions", str_local+'.csv'))
     elif HOW_TO_SAVE == 'PICKLE':
         to_save = {"config": config,
                    "features": test.feature_names,
