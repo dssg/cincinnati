2ac3e7126d00ce33f2e1a014dc511a137c6965ae
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-19 10:25:39 -0600

    removes old bash script

--- blight_risk_prediction/features/features.sh
+++
@@ -1,4 +0,0 @@
-#Run Python script to generate features
-python -m blight_risk_prediction.features.featurebot
-#to generate features for if an inspection happens in July 01, 2015
-python -m blight_risk_prediction.features.featurebot 20Jul2015
