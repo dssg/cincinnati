ba854de1f1e53133cdc76f275ce74b7663299619
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-19 16:04:21 -0600

    changes max dist to 3km

--- etl/three11/parcels_to_three11.sql
+++ etl/three11/parcels_to_three11.sql
@@ -7,5 +7,5 @@
 CREATE TABLE parcels_to_three11 AS (
     SELECT parcels.parcelid, three.service_request_id, ST_Distance(parcels.geom, three.geom)/1000 AS dist_km
     FROM shape_files.parcels_cincy AS parcels
-    JOIN public.three11 AS three ON ST_DWithin(parcels.geom, three.geom, 5000) --meters
+    JOIN public.three11 AS three ON ST_DWithin(parcels.geom, three.geom, 3000) --meters
 );
