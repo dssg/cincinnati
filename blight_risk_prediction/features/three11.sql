3a029c5738f8271a153d668fa5cd06cf714e58cb
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-19 14:53:59 -0600

    matching parcels to 311 calls

--- blight_risk_prediction/features/three11.sql
+++
@@ -1,6 +0,0 @@
---Script for computing 311 related features
-
---Should I make a distinction between calls close to a parcel and the ones
---INSIDE the parcel?
-
---Step one: for each parcel, give me the 311 calls within X kilometers
