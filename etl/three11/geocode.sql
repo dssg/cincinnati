2e44a9abb057539df8892f4d54624cbc72c74925
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-19 14:09:32 -0600

    sql code to geocode three11

---
+++ etl/three11/geocode.sql
@@ -0,0 +1,17 @@
+--Create temporary table with a geom field
+CREATE TEMP TABLE geo_three11 AS (
+    select *, ST_SetSRID(ST_MakePoint(x_coord, y_coord), 3735) AS geom from public.three11
+);
+
+--Drop original table
+DROP TABLE IF EXISTS public.three11;
+
+--Create new table with the same name as the original one
+CREATE TABLE public.three11 AS(
+
+SELECT geo_three11.*
+FROM    geo_three11,
+        shape_files.cinc_city_boundary AS boundary
+WHERE ST_Within(geo_three11.geom, boundary.geom)
+
+)
