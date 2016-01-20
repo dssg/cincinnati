506e1b5660686212d293270ff184cde63637642a
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-20 15:31:18 -0600

    script for creating 1 month 311 calls for each inspection

--- blight_risk_prediction/features/three11.sql
+++ blight_risk_prediction/features/three11.sql
@@ -5,10 +5,10 @@
 
 --Join the parcels and complains table with the inspections table
 --Generate one rows for each complain within X months on each inspection
-WITH complains_in_time_window(
-    SELECT *
-    FROM features.parcels_inspections AS insp --change this
-    JOIN parcels_and_complains AS pnc
+CREATE TABLE AS features.three11_for_inspections_1_month(
+    SELECT insp.parcel_id, insp.inspection_date, pnc.requested_datetime, pnc.dist_km, pnc.service_code, pnc.agency_responsible, insp.inspection_date -  pnc.requested_datetime AS diff
+    FROM features.parcels_inspections AS insp --change this when you move it to featurebot
+    JOIN parcels_three11_view AS pnc
     ON insp.parcel_id=pnc.parcelid
     AND (insp.inspection_date - '1 month'::interval) >= pnc.requested_datetime --complain date should be X months before insepction at most
     AND pnc.requested_datetime <= insp.inspection_date --and don't give me complains past the inspection date
