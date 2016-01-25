4043b37351ba8aae84411dd9811bca9110e6d1e9
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-25 15:10:56 -0600

    updates notebooks, fire etl and fire geocoding

--- etl/fire/clean.py
+++ etl/fire/clean.py
@@ -7,13 +7,16 @@ path_to_data_folder = data_folder.for_file(__file__)
 os.chdir(os.path.join(path_to_data_folder, 'tmp'))
 print 'Working in folder: %s' % path_to_data_folder
 
-#Load csv file
-df = pd.read_csv("fire_1997_2014_CFS.csv")
+#Load csv file and parse DATE column
+df = pd.read_csv("fire_1997_2014_CFS.csv", parse_dates=['DATE'])
 
 #Lowercase column names
 df.columns = df.columns.map(lambda s: s.lower().replace('#', ''))
 #signal column has some extra spaces
 df.signal = df.signal.map(lambda s: s.replace(' ', ''))
+#We are only using data starting from 2005
+indexes = [i.year >= 2005 for i in df.date]
+df = df[indexes]
 
 #Save data frame
 df.to_csv('fire.csv', index=False)
---
+++ etl/fire/geocode.py
@@ -0,0 +1,6 @@
+from omgeo import Geocoder
+import pandas as pd
+
+g = Geocoder()
+
+df = pd.read_csv('fire.csv')
