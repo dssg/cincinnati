7eca1e3b746962c08da01280720d5c71bebcfd67
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-29 12:27:51 -0600

    moves utility functions to a separate file

---
+++ blight_risk_prediction/features/feature_utils.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from string import Template
+import os
+#This file provides generic functions
+#to generate spatiotemporal features
+
+def create_events_3months_table(con, schema, table_name, date_column):
+    path_to_template = os.path.join(os.environ['ROOT_FOLDER'],
+                    'blight_risk_prediction',
+                    'features',
+                    'events_xmonths.template.sql')
+    #Load template with SQL statement
+    with open(path_to_template, 'r') as f:
+        sql_script = Template(f.read())
+    #Replace values in template
+    sql_script = sql_script.substitute(TABLE_NAME=table_name, DATE_COLUMN=date_column)
+    #Run the code using the connection
+    #this is going to take a while
+    try:
+        con.cursor().execute(sql_script)
+    except Exception, e:
+        con.rollback()
+        con.cursor().execute("SET SCHEMA '{}'".format(schema))
+        print 'Failed to create 3 month table. {}'.format(e)
+
+def compute_frequency_features(con, table_name, ids=['parcel_id', 'inspection_date'],
+                               columns):
+    ids = [ids] if type(ids)==str else ids
+    columns = [columns] if type(columns)==str else columns
+
+    df = pd.read_sql('SELECT * FROM events_3months_{}}'.format(table_name), con)
+    ids_series = [df[i] for i in ids]
+    cols_series = [df[i] for i in columns]
+    #Group by parcel_id and inspection_date. Make columns with counts
+    #for some columns
+    cross = pd.crosstab(ids_series, cols_series)
+    #Rename columns to avoid capital letters and spaces
+    cross.columns = cross.columns.map(lambda s: s.replace(' ', '_').lower())
+    return cross
--- blight_risk_prediction/features/fire.py
+++ blight_risk_prediction/features/fire.py
@@ -1,42 +1,4 @@
-import pandas as pd
-from dstools.db import uri
-from sqlalchemy import create_engine
-from string import Template
-import os
-
-engine = create_engine(uri)
-
-path_to_template = os.path.join(os.environ['ROOT_FOLDER'],
-                    'blight_risk_prediction',
-                    'features',
-                    'events_xmonths.template.sql')
-table_name = 'fire'
-date_column = 'date'
-
-def create_events_3months_table(con, schema):
-    #Load template with SQL statement
-    with open(path_to_template, 'r') as f:
-        sql_script = Template(f.read())
-    #Replace values in template
-    sql_script = sql_script.substitute(TABLE_NAME=table_name, DATE_COLUMN=date_column)
-    #Run the code using the connection
-    #this is going to take a while
-    try:
-        con.cursor().execute(sql_script)
-    except Exception, e:
-        con.rollback()
-        con.cursor().execute("SET SCHEMA '{}'".format(schema))
-        print 'Failed to create 3 month table. {}'.format(e)
-
-def compute_frequency_features(con, schema):
-    df = pd.read_sql('SELECT * FROM events_3months_fire', con)
-    #Group by parcel_id and inspection_date. Make columns with counts
-    #for some columns
-    cross = pd.crosstab([df.parcel_id, df.inspection_date],
-                         df.signal)
-    #Rename columns to avoid capital letters and spaces
-    cross.columns = cross.columns.map(lambda s: s.replace(' ', '_').lower())
-    return cross
+from feature_utils import create_events_3months_table, compute_frequency_features
 
 def make_fire_features(con):
     """
@@ -56,14 +18,17 @@ def make_fire_features(con):
     cur.execute('SELECT current_schema;')
     schema = cur.fetchone()[0]
 
+    table_name = 'fire'
+    date_column = 'date'
+
     #Create table with events that happened before 3 months of inspection database
     #If table exists, send message and skip
-    create_events_3months_table(con, schema)
+    create_events_3months_table(con, schema, table_name, date_column)
 
     #Use the recently created table to compute features.
     #Group rows by parcel_id and inspection_date
     #For now, just perform counts on the categorical variables
     #More complex features could combine the distance value
     #as well as interacting features
-    df = compute_frequency_features(con, schema)
+    df = compute_frequency_features(con, table_name, columns='signal')
     return df
