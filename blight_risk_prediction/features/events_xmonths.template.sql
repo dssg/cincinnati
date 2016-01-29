d41569caf36f630c14c48c82a543ae6c8e2be0c3
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-29 00:02:31 -0600

    adds missing schemas in tempalte

--- blight_risk_prediction/features/events_xmonths.template.sql
+++ blight_risk_prediction/features/events_xmonths.template.sql
@@ -10,12 +10,12 @@ CREATE TABLE events_3months_$TABLE_NAME AS (
         insp.parcel_id, insp.inspection_date,
         p2a.dist_km,
         $TABLE_NAME.*
-    FROM features.parcels_inspections AS insp
-    JOIN parcel2address AS p2a
+    FROM parcels_inspections AS insp
+    JOIN default.parcel2address AS p2a
     USING (parcel_id)
-    JOIN address
+    JOIN default.address
     ON address_id=address.id
-    JOIN $TABLE_NAME
+    JOIN default.$TABLE_NAME
     ON address.address=$TABLE_NAME.address --THIS IS SLOW, maybe change for an ID or simply add index
     AND (insp.inspection_date - '3 month'::interval) <= $TABLE_NAME.date
     AND $TABLE_NAME.date <= insp.inspection_date
