32495658a5a64f79a7295883222095041e1b3064
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-31 16:14:56 -0600

    updates notebooks

--- blight_risk_prediction/features/feature_utils.py
+++ blight_risk_prediction/features/feature_utils.py
@@ -25,6 +25,27 @@ def create_inspections_address_xmonths_table(con, schema, table_name, date_colum
         con.cursor().execute("SET SCHEMA '{}'".format(schema))
         print 'Failed to create {} month table. {}'.format(n_months, e)
 
+def create_inspections_latlong_xmonths_table(con, schema, table_name, date_column, n_months=3):
+    path_to_template = os.path.join(os.environ['ROOT_FOLDER'],
+                    'blight_risk_prediction',
+                    'features',
+                    'inspection_address_xmonths.template.sql')
+    #Load template with SQL statement
+    with open(path_to_template, 'r') as f:
+        sql_script = Template(f.read())
+    #Replace values in template
+    sql_script = sql_script.substitute(TABLE_NAME=table_name,
+                                       DATE_COLUMN=date_column,
+                                       N_MONTHS=n_months)
+    #Run the code using the connection
+    #this is going to take a while
+    try:
+        con.cursor().execute(sql_script)
+    except Exception, e:
+        con.rollback()
+        con.cursor().execute("SET SCHEMA '{}'".format(schema))
+        print 'Failed to create {} month table. {}'.format(n_months, e)
+
 def compute_frequency_features_from_table(con, table_name, columns, ids=['parcel_id', 'inspection_date']):
     ids = [ids] if type(ids)==str else ids
     columns = [columns] if type(columns)==str else columns
