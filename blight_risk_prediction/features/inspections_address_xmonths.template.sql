a8264db5b256cefa0516bd5daba0ed57de769576
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-31 18:11:03 -0600

    updates templates

--- blight_risk_prediction/features/inspections_address_xmonths.template.sql
+++ blight_risk_prediction/features/inspections_address_xmonths.template.sql
@@ -5,7 +5,7 @@
 --inspection date
 
 --This script is intended to be used as a template for various tables
-CREATE TABLE insp_$N_MONTHSmonths_$TABLE_NAME AS (
+CREATE TABLE insp_${N_MONTHS}months_${TABLE_NAME} AS (
     SELECT
         insp.parcel_id, insp.inspection_date,
         p2a.dist_km,
@@ -15,9 +15,9 @@ CREATE TABLE insp_$N_MONTHSmonths_$TABLE_NAME AS (
     USING (parcel_id)
     JOIN public.address
     ON address_id=address.id
-    JOIN public.$TABLE_NAME AS event
+    JOIN public.${TABLE_NAME} AS event
     ON address.id=event.address_id
-    AND (insp.inspection_date - '$N_MONTHS month'::interval) <= event.$DATE_COLUMN
-    AND event.$DATE_COLUMN <= insp.inspection_date
+    AND (insp.inspection_date - '${N_MONTHS} month'::interval) <= event.${DATE_COLUMN}
+    AND event.${DATE_COLUMN} <= insp.inspection_date
 );
 
