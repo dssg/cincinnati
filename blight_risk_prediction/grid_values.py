048e9af188bef08f0c27c72d862dbbdd32f1bed4
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-02-05 11:54:40 -0600

    adds more grids

--- blight_risk_prediction/grid_values.py
+++ blight_risk_prediction/grid_values.py
@@ -19,6 +19,21 @@ big = {
     "sklearn.svm.SVC": {
         'C': [0.00001,0.0001,0.001,0.01,0.1,1,10],
         'kernel': ['linear']
+    },
+
+    "sklearn.ensemble.ExtraTreesClassifier": {
+        'n_estimators': [1,10,100,1000,10000],
+        'criterion' : ['gini', 'entropy'],
+        'max_depth': [1,5,10,20,50,100],
+        'max_features': ['sqrt','log2'],
+        'min_samples_split': [2,5,10]
+    },
+
+    "sklearn.ensemble.GradientBoostingClassifier": {
+        'n_estimators': [1,10,100,1000,10000],
+        'learning_rate' : [0.001,0.01,0.05,0.1,0.5],
+        'subsample' : [0.1,0.5,1.0],
+        'max_depth': [1,3,5,10,20,50,100]
     }
 }
 
@@ -43,6 +58,21 @@ medium = {
     "sklearn.svm.SVC": {
         'C': [0.001,0.01,0.1,1,10],
         'kernel': ['linear']
+    },
+
+    "sklearn.ensemble.ExtraTreesClassifier": {
+        'n_estimators': [1,10,100],
+        'criterion' : ['gini', 'entropy'],
+        'max_depth': [10,50,100],
+        'max_features': ['sqrt','log2'],
+        'min_samples_split': [2,5,10]
+    },
+
+    "sklearn.ensemble.GradientBoostingClassifier": {
+        'n_estimators': [1,10,100],
+        'learning_rate' : [0.01,0.05,0.1],
+        'subsample' : [0.1,0.5,1.0],
+        'max_depth': [5,10,50]
     }
 }
 
@@ -65,5 +95,15 @@ small = {
     "sklearn.svm.SVC": {
         'C': [0.01,0.1,1,10],
         'kernel': ['linear']
+    },
+
+    "sklearn.ensemble.ExtraTreesClassifier": {
+        'n_estimators': [10,100],
+        'max_features': ['sqrt','log2'],
+    },
+
+    "sklearn.ensemble.GradientBoostingClassifier": {
+        'n_estimators': [10,100],
+        'learning_rate' : [0.05,0.1],
     }
 }
