05fc26699828a087dd73a089607aa2a49fe7bd11
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-02-09 21:10:18 -0600

    updates to notebooks and experiments

---
+++ experiments/kdd/run_training_size.sh
@@ -0,0 +1,10 @@
+PATH_TO_MODEL="$ROOT_FOLDER/blight_risk_prediction"
+PATH_TO_EXP="$ROOT_FOLDER/experiments"
+CORES=4
+
+$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/freq_features_1years.yaml
+$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/freq_features_2years.yaml
+$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/freq_features_3years.yaml
+$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/freq_features_4years.yaml
+$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/freq_features_5years.yaml
+$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/freq_features_6years.yaml
