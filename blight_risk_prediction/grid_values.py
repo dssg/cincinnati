d6b1d70e4e9e532de01a872b46e245aba87bb82d
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-02-10 20:29:16 -0600

    updates nb

--- blight_risk_prediction/grid_values.py
+++ blight_risk_prediction/grid_values.py
@@ -1,6 +1,7 @@
 big = {
     "sklearn.ensemble.RandomForestClassifier": {
         'n_estimators': [1,10,100,1000,10000],
+	'criterion' : ['gini', 'entropy'],
         'max_depth': [1,5,10,20,50,100],
         'max_features': ['sqrt','log2'],
         'min_samples_split': [2,5,10]
@@ -40,6 +41,7 @@ big = {
 medium = {
     "sklearn.ensemble.RandomForestClassifier": {
         'n_estimators': [1,10,100],
+	'criterion' : ['gini', 'entropy'],
         'max_depth': [10,50,100],
         'max_features': ['sqrt','log2'],
         'min_samples_split': [2,5,10]
@@ -93,7 +95,7 @@ small = {
     },
     
     "sklearn.svm.SVC": {
-        'C': [0.01,0.1,1,10],
+        'C': [0.01,0.1,1],
         'kernel': ['linear']
     },
 
--- experiments/kdd/run_spatiotemporal_params.sh
+++ experiments/kdd/run_spatiotemporal_params.sh
@@ -1,13 +1,13 @@
 PATH_TO_MODEL="$ROOT_FOLDER/blight_risk_prediction"
-PATH_TO_EXP="$ROOT_FOLDER/experiments"
+PATH_TO_EXP="$ROOT_FOLDER/experiments/kdd"
 CORES=4
 
-$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/all_freq_features_1000m_3months.yaml
-$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/all_freq_features_1000m_6months.yaml
-$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/all_freq_features_700m_3months.yaml
-$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/all_freq_features_700m_6months.yaml
-$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/all_freq_features_400m_3months.yaml
-$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/all_freq_features_400m_6months.yaml
-$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/all_freq_features_1000m_3months.yaml
-$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/all_freq_features_1000m_6months.yaml
-$PATH_TO_MODEL/model.py -c $PATH_TO_EXP/all_freq_features_1000m_9months.yaml
+$PATH_TO_MODEL/model.py -n $CORES -c $PATH_TO_EXP/all_freq_features_1000m_3months.yaml
+$PATH_TO_MODEL/model.py -n $CORES -c $PATH_TO_EXP/all_freq_features_1000m_6months.yaml
+$PATH_TO_MODEL/model.py -n $CORES -c $PATH_TO_EXP/all_freq_features_700m_3months.yaml
+$PATH_TO_MODEL/model.py -n $CORES -c $PATH_TO_EXP/all_freq_features_700m_6months.yaml
+$PATH_TO_MODEL/model.py -n $CORES -c $PATH_TO_EXP/all_freq_features_400m_3months.yaml
+$PATH_TO_MODEL/model.py -n $CORES -c $PATH_TO_EXP/all_freq_features_400m_6months.yaml
+$PATH_TO_MODEL/model.py -n $CORES -c $PATH_TO_EXP/all_freq_features_1000m_3months.yaml
+$PATH_TO_MODEL/model.py -n $CORES -c $PATH_TO_EXP/all_freq_features_1000m_6months.yaml
+$PATH_TO_MODEL/model.py -n $CORES -c $PATH_TO_EXP/all_freq_features_1000m_9months.yaml
