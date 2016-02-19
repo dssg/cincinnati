007b0286aa23f0d40714b46670054c93a5cb96aa
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-02-19 14:35:23 -0600

    changes logger level to INFO

--- blight_risk_prediction/util.py
+++ blight_risk_prediction/util.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sqlalchemy import create_engine
 import datetime
+from dstools.config import load
 
 logging.config.dictConfig(load('logger_config.yaml'))
 logger = logging.getLogger()
