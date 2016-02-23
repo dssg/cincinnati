6d4a05bf5af5382fd65c91efa73c752ef89a719b
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-02-23 17:21:32 -0600

    model.py creates all folder that needs in case they dont exist

--- blight_risk_prediction/model.py
+++ blight_risk_prediction/model.py
@@ -32,18 +32,20 @@ logger = logging.getLogger()
 
 #Where to save test set predictions
 path_to_predictions = os.path.join(os.environ['OUTPUT_FOLDER'], "predictions")
-
 #Where to pickle models
 path_to_pickled_models = os.path.join(os.environ['OUTPUT_FOLDER'], "pickled_models")
 #Where to pickle scales
 path_to_pickled_scalers = os.path.join(os.environ['OUTPUT_FOLDER'], "pickled_scalers")
 #Where to pickle imputers
 path_to_pickled_imputers = os.path.join(os.environ['OUTPUT_FOLDER'], "pickled_imputers")
-
 #Where to dump train and testing sets
 path_to_dumps = os.path.join(os.environ['OUTPUT_FOLDER'], "dumps")
+
+dirs = [path_to_predictions, path_to_pickled_models,
+         path_to_pickled_scalers, path_to_pickled_imputers,
+         path_to_dumps]
 #Make directories if they don't exist
-for directory in [path_to_pickles, path_to_predictions, path_to_dumps]:
+for directory in dirs:
     if not os.path.exists(directory):
         os.makedirs(directory)
 
