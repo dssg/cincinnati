54c4051544deb463d4d3c70fb4536eaf4c59632c
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-02-24 15:22:36 -0600

    adds environment sample

---
+++ environment_sample.sh
@@ -0,0 +1,5 @@
+export ROOT_FOLDER="/path/to/repo/"
+export DATA_FOLDER="/path/to/data/"
+export OUTPUT_FOLDER="/path/to/output/"
+#Add lib folder to PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$ROOT_FOLDER/lib_cinci
