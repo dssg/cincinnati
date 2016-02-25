1a7e944935296c6f33ea6f2efca7ac7dca1cf440
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-02-25 12:58:29 -0600

    adds dstools functions to lib_cinci

---
+++ lib_cinci/lib_cinci/config.py
@@ -0,0 +1,18 @@
+import os
+import yaml
+
+#load the config.yaml file
+folder = os.environ['ROOT_FOLDER']
+name = 'config.yaml'
+path = "%s/%s" % (folder, name)
+f = open(path, 'r')
+text = f.read()
+main = yaml.load(text)
+
+def load(name):
+    folder = os.environ['ROOT_FOLDER']
+    path = "%s/%s" % (folder, name)
+    with open(path, 'r') as f:
+        text = f.read()
+    dic = yaml.load(text)
+    return dic
