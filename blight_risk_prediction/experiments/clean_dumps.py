cd2c8b060157955539733e8a0228b4ea8c0726a5
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-02-26 15:49:08 -0600

    adds experiments for spring 2016 list

---
+++ blight_risk_prediction/experiments/clean_dumps.py
@@ -0,0 +1,8 @@
+'''
+    Using the --pickle option in model.py will dump the
+    model, scaler and imputer objects. Use this to only keep
+    the top_n models from each experiment and trash the rest.
+'''
+
+#Top n models to keep from each experiment
+n = 20
