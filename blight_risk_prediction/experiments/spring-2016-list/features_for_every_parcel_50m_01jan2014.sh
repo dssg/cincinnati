458131386a9b08808d36e9ffb4fe296add586cd8
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-03-10 16:58:42 -0600

    removes non-spatiotemporal features from some scripts

--- blight_risk_prediction/experiments/spring-2016-list/features_for_every_parcel_50m_01jan2014.sh
+++ blight_risk_prediction/experiments/spring-2016-list/features_for_every_parcel_50m_01jan2014.sh
@@ -3,9 +3,6 @@
 #3months, 6months, 9months
 PATH_TO_BOT="$ROOT_FOLDER/blight_risk_prediction/features"
 
-#non-spatiotemporal features
-$PATH_TO_BOT/featurebot.py -d 01jan2014 -f tax,crime_agg,named_entities,house_type,parc_area,parc_year,census_2010
-
 $PATH_TO_BOT/featurebot.py -d 01jan2014 -f three11,permits,crime,fire,sales -m 3 -md 50
 $PATH_TO_BOT/featurebot.py -d 01jan2014 -f three11,permits,crime,fire,sales -m 6 -md 50
 $PATH_TO_BOT/featurebot.py -d 01jan2014 -f three11,permits,crime,fire,sales -m 9 -md 50
