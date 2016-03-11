acc73b607cf0385c5dd63c3a589e940d906806c6
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-03-11 15:37:14 -0600

    adds full path to config files in run experiments script

--- blight_risk_prediction/experiments/spring-2016-list/run_experiments.sh
+++ blight_risk_prediction/experiments/spring-2016-list/run_experiments.sh
@@ -2,13 +2,14 @@
 CORES=8
 PATH_TO_MODEL=$ROOT_FOLDER/blight_risk_prediction/model.py
 PATH_TO_CLEAN=$ROOT_FOLDER/blight_risk_prediction/experiments/clean_dumps.py
+PATH_TO_EXP=$ROOT_FOLDER/blight_risk_prediction/experiments/spring-2016-list
 
-$PATH_TO_MODEL -c experiments/spring-2016-list/all_freq_features_1000m_3months_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 1';
-$PATH_TO_MODEL -c experiments/spring-2016-list/all_freq_features_50m_3months_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 2';
-$PATH_TO_MODEL -c experiments/spring-2016-list/all_freq_features_all_spatiotemporal_params_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 3';
-$PATH_TO_MODEL -c experiments/spring-2016-list/only_agg_and_parcel_features_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 4';
-$PATH_TO_MODEL -c experiments/spring-2016-list/only_agg_and_parcel_features_12_13.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 5';
-$PATH_TO_MODEL -c experiments/spring-2016-list/only_freq_features_50m_3months_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 6';
-$PATH_TO_MODEL -c experiments/spring-2016-list/only_freq_features_50m_3months_12_13.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 7';
-$PATH_TO_MODEL -c experiments/spring-2016-list/only_parcel_features_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 8';
-$PATH_TO_MODEL -c experiments/spring-2016-list/only_parcel_features_12_13.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 9';
+$PATH_TO_MODEL -c $PATH_TO_EXP/all_freq_features_1000m_3months_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 1';
+$PATH_TO_MODEL -c $PATH_TO_EXP/all_freq_features_50m_3months_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 2';
+$PATH_TO_MODEL -c $PATH_TO_EXP/all_freq_features_all_spatiotemporal_params_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 3';
+$PATH_TO_MODEL -c $PATH_TO_EXP/only_agg_and_parcel_features_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 4';
+$PATH_TO_MODEL -c $PATH_TO_EXP/only_agg_and_parcel_features_12_13.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 5';
+$PATH_TO_MODEL -c $PATH_TO_EXP/only_freq_features_50m_3months_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 6';
+$PATH_TO_MODEL -c $PATH_TO_EXP/only_freq_features_50m_3months_12_13.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 7';
+$PATH_TO_MODEL -c $PATH_TO_EXP/only_parcel_features_12_13_all_parcels.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 8';
+$PATH_TO_MODEL -c $PATH_TO_EXP/only_parcel_features_12_13.yaml -n $CORES -p && $PATH_TO_CLEAN && mailme 'exp 9';
