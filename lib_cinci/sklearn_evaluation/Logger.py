b05d90c6fc059a3fe66b71a219d39eca9a1ef6a8
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-04-06 11:56:10 -0500

    adds method to find best model for a given experiment

--- lib_cinci/sklearn_evaluation/Logger.py
+++ lib_cinci/sklearn_evaluation/Logger.py
@@ -1,14 +1,17 @@
 from pymongo import MongoClient
 from utils import get_model_name
 import datetime
+import pymongo
 
 class Logger:
     def __init__(self, host, db, collection):
-        #Start connection
         client = MongoClient(host)
         self.collection = client[db][collection]
 
     def log_model(self, model, **keywords):
+        '''
+            Log a model to the database
+        '''
         params = model.get_params()
         name = get_model_name(model)
         dt = datetime.datetime.utcnow() 
@@ -18,6 +21,17 @@ class Logger:
         return str(inserted_id)
 
     def experiment_exists(self, experiment_name):
+        '''
+            Check if an experiment already exists
+        '''
         model = {'experiment_name':experiment_name}
         model_count = self.collection.find(model).count()
         return model_count > 0
+
+    def get_best_from_experiment(self, experiment_name, key):
+        '''
+            Returns entry for the best model given an experiment_name
+            and a key
+        '''
+        return (self.collection.find({"experiment_name": experiment_name})
+                    .sort("prec_at_1", pymongo.DESCENDING)[0])
