554825db0354132a6972214814ac54a02d8da6be
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-04-13 14:12:31 -0500

    improves processing script to prevent repeated indexes and adds missing spatial index

--- etl/three11/process_table.sql
+++ etl/three11/process_table.sql
@@ -1,15 +1,19 @@
 --Create unique id
-ALTER TABLE three11 ADD id SERIAL;
+ALTER TABLE three11_2 ADD id SERIAL;
 
 --make it primary key, this will allso create an index
-ALTER TABLE three11 ADD PRIMARY KEY (id);
+ALTER TABLE three11_2 ADD PRIMARY KEY (id);
 
 --add column for geometry point
-ALTER TABLE three11 ADD geom geometry;
-
---compute geom based on lat,long
-UPDATE three11
-    SET geom=ST_Transform(ST_SetSRID(ST_MakePoint(longitude, latitude),4326), 3735);
+ALTER TABLE three11_2 ADD geom geometry;
 
 --create index on date column
-CREATE INDEX ON three11 (requested_datetime);
+CREATE INDEX three11_temporal_index ON three11_2 (requested_datetime);
+
+--create index on geom column
+CREATE INDEX three11_spatial_index ON three11_2 USING GIST (geom);
+
+--compute geom based on lat,long
+UPDATE three11_2
+    SET geom=ST_Transform(ST_SetSRID(ST_MakePoint(longitude, latitude),4326), 3735)
+    WHERE geom IS NULL;
