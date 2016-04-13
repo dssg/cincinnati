4e79951c7d485f4983b228012d77c1699920d850
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-04-13 12:36:21 -0500

    update script working and new etl for 311 in progress

---
+++ etl/three11/three11_new.sh
@@ -0,0 +1,4 @@
+#Path to update script
+UPDATE_SCRIPT=$ROOT_FOLDER/lib_cinci/data_updater/update.py
+
+$UPDATE_SCRIPT $ROOT_FOLDER/etl/three11/update.yaml
--- lib_cinci/data_updater/update.py
+++ lib_cinci/data_updater/update.py
@@ -1,37 +1,76 @@
+#!/usr/bin/env python
+import logging
+import logging.config
+import argparse
 import urllib2
-from lib_cinci.db import uri
-import dataset
+import os
+
+import dataset #this is really slow since it gets the entire db schema when connection
 import pandas as pd
+import yaml
 
+from lib_cinci.db import uri
+from lib_cinci.config import load
+from lib_cinci import data_folder
 
-#Step zero: read from yaml file
-url = 'https://data.cincinnati-oh.gov/api/views/4cjh-bm8b/rows.csv?accessType=DOWNLOAD'
-schema = 'public'
-table_name = 'three11'
-db_column_name = 'requested_datetime'
-file_column_name = 'REQUESTED_DATETIME'
+if __name__ == '__main__':
+    #Configure logger
+    logging.config.dictConfig(load('logger_config.yaml'))
+    logger = logging.getLogger()
 
-file_name = 'data.csv'
+    #Step zero: read from yaml file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("update_file", help="Path to yaml file with configuration parameters")
+    args = parser.parse_args()
 
-#Step one: download file from remote server
-data_file = urllib2.urlopen(url)
-#Dowload file replacing it if already exists
-with open(file_name,'wb') as output:
-    output.write(data_file.read())
+    with open(args.update_file, 'r') as f:
+        params = yaml.load(f)
 
-#Step two: check most recent entry in the database
-db = dataset.connect(uri)
-table = db[table_name]
-db_most_recent = table.find_one(order_by='-'+db_column_name)[db_column_name]
+    db_column = params['storage']['column']
+    file_column = params['source']['column']
+    #schema = 'public'
 
-#Step three: subset the file to include new entries
-df = pd.read_csv(file_name)
-df[file_column_name] = pd.to_datetime(df[file_column_name])
-is_new = df[file_column_name] > db_most_recent 
-new_entries = df[is_new]
+    #Folder to use for I/O
+    folder = data_folder.for_file(args.update_file)
+    os.chdir(folder)
+    logger.info('Using {} for I/O operations'.format(folder))
 
-print '{:,d} new enties'.format(new_entries.shape[0])
+    #Step one: download file from remote server if user provided url
+    try:
+        url = params['source']['url']
+    except Exception, e:
+        logger.info('URL was not present in the configuration file...')
+    else:
+        logger.info('Downloading file...'.format(folder))
+        data_file = urllib2.urlopen(url)
+        #Dowload file replacing it if already exists
+        with open(params['source']['filename'],'wb') as output:
+            output.write(data_file.read())
 
-#Step four: save new entries in a csv file
-new_entries.to_csv('diff.csv', index=False)
+    #Step two: check most recent entry in the database
+    db = dataset.connect(uri)
+    table_name = params['storage']['table']
+    table = db[table_name]
+    
+    #Query the database, if returns None, the table doesn't exist
+    most_recent_row = table.find_one(order_by='-'+db_column)
+    if most_recent_row is None:
+        db_most_recent = None
+        logger.info('Table does not exist, diff file will be a copy of source file')
+    else:
+        db_most_recent = most_recent_row[db_column]
+        logger.info('Most recent record in database is: {}'.format(db_most_recent))
+    
+    #Step three: load and subset the file to include new entries
+    logger.info('Loading {}'.format(params['source']['filename']))
+    df = pd.read_csv(params['source']['filename'])
+    df[file_column] = pd.to_datetime(df[file_column])
 
+    #Subset only if db_most_recent has a value    
+    new_entries = df[df[file_column] > db_most_recent] if db_most_recent else df
+    logger.info('{:,d} new enties'.format(new_entries.shape[0]))
+    
+    #Step four: save new entries in a csv file
+    diff_file = 'tmp/diff.csv'
+    new_entries.to_csv(diff_file, index=False)
+    logger.info('New entries saved in {}'.format(os.path.join(folder, diff_file)))
