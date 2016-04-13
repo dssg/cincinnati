15585d7ff903d761619b0241615844674c091e25
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-04-13 16:48:20 -0500

    parcel2three11.sql now updates the table using last_updated_event table

--- etl/last_updated_event.sql
+++ etl/last_updated_event.sql
@@ -1,7 +1,7 @@
 --This table keeps track of the last record
 --used for computing distances to avoid recomputations
 --after datasets are updated
-CREATE TABLE last_updated_record (
+CREATE TABLE last_updated_event (
     table_name varchar(50) NOT NULL UNIQUE,
-    record_id integer DEFAULT 0
+    event_id integer DEFAULT 0
 );
--- etl/three11/parcel2three11.sql
+++ etl/three11/parcel2three11.sql
@@ -3,14 +3,39 @@
 --1 US survey foot = 1200⁄3937 m
 --1000 m ~ 3281 US survey foot
 -- X [US Survey foot] / 3.281 ~ Y m
-
---match parcels to 311 events
-CREATE TABLE parcel2three11 AS (
-    SELECT parcels.parcelid AS parcel_id, three11.id AS three11_id, ST_Distance(parcels.geom, three11.geom)/3.281 AS dist_m
-    FROM shape_files.parcels_cincy AS parcels
-    JOIN three11
-    ON ST_DWithin(parcels.geom, three11.geom, 3281)
+INSERT INTO last_updated_event
+    (table_name, event_id)
+    SELECT 'three11_2', 0
+WHERE NOT EXISTS (
+    SELECT table_name FROM last_updated_event WHERE table_name = 'three11_2'
 );
 
+--Create table if doesn't exist
+CREATE TABLE IF NOT EXISTS parcel2three11_2 (
+    parcel_id varchar(20) NOT NULL,
+    event_id int4 NOT NULL,
+    dist_m float8 NOT NULL
+);
+--Create indexes on parcel_id and three11_id
 CREATE INDEX three11_parcel_id_index ON parcel2three11 (parcel_id);
 CREATE INDEX three11_event_id_index ON parcel2three11 (three11_id);
+    
+--Insert new rows into table
+INSERT INTO parcel2three11_2(
+    parcel_id,
+    event_id,
+    dist_m
+)
+WITH new_records AS (
+    SELECT * FROM three11_2 WHERE id > (SELECT event_id FROM last_updated_event WHERE table_name='three11_2')
+)
+--subselect table with rows that have an id greater than the one stored in last_updated_event table
+SELECT parcels.parcelid AS parcel_id, new_records.id AS event_id, ST_Distance(parcels.geom, new_records.geom)/3.281 AS dist_m
+FROM shape_files.parcels_cincy AS parcels
+JOIN new_records
+ON ST_DWithin(parcels.geom, new_records.geom, 3281)
+
+--Update last_updated_event table
+UPDATE last_updated_event
+    SET event_id = (SELECT MAX(id) FROM three11_2)
+    WHERE table_name = 'three11_2';
