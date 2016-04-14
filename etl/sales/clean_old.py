cd5535337bdeeac65d75a7c83ee9ee7d329aaffb
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-04-14 18:48:46 -0500

    script to print csv stats

--- etl/sales/clean_old.py
+++ etl/sales/clean_old.py
@@ -8,12 +8,19 @@ os.chdir(os.path.join(path_to_data_folder, 'tmp'))
 print 'Working in folder: %s' % path_to_data_folder
 
 #Load csv file
-df = pd.read_csv("diff_sales.csv", parse_dates=['date_of_sale'],
+df = pd.read_csv("salesinfo.csv", parse_dates=['DATESALE'],
                 dtype={'st_number': int})
 print 'Raw file has {:,d} rows and {:,d} columns'.format(*df.shape)
+#Remove empty column
+df.drop('Unnamed: 0', axis=1, inplace=True)
 
 #Lowercase column names
 df.columns = df.columns.map(lambda s: s.lower())
+
+#We are only using data starting from 2005
+indexes = [i.year >= 2005 for i in df.datesale]
+df = df[indexes]
+print 'Subset from 2005 has {:,d} rows and {:,d} columns'.format(*df.shape)
 
 #Parse address data
 def format_address(x):
@@ -27,9 +34,12 @@ def format_address(x):
 df['address'] = [format_address(x) for x in df.itertuples()]
 df.drop(['st_dir', 'st_name', 'st_name'], axis=1, inplace=True)
 
+#Rename some columns
+df.rename(columns={'zipcode': 'zip'}, inplace=True)
+
 #Add columns for geocoding
 df['city'] = 'CINCINNATI'
 df['state'] = 'OHIO'
 
 #Save data frame
-df.to_csv('diff_sales_clean.csv', index=False)
+df.to_csv('sales_clean.csv', index=False)
