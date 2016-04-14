74e95d6bd295079a5e213a992df4c799a3122eb3
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-04-14 16:12:48 -0500

    about to change sales etl

--- etl/sales/sales_new.sh
+++ etl/sales/sales_new.sh
@@ -15,12 +15,6 @@ UPDATE_SCRIPT=$ROOT_FOLDER/lib_cinci/data_updater/update.py
 #Create diff file with entries to upload to the database
 $UPDATE_SCRIPT $ROOT_FOLDER/etl/sales/update.yaml
 
-#Convert raw data to tsv
-#REMOVE THIS extract.py and transform.py
-#python "$ROOT_FOLDER/etl/sales/extract.py" "$SALES_FOLDER/salesinfo.txt" > "$TMP_FOLDER/salesinfo.tsv"
-#transform data and export to csv
-#python "$ROOT_FOLDER/etl/sales/transform.py" "$TMP_FOLDER/salesinfo.tsv" "$TMP_FOLDER/salesinfo.csv"
-
 #Clean dataset
 echo 'Cleaninng data...'
 python "$ROOT_FOLDER/etl/sales/clean.py"
@@ -33,14 +27,14 @@ python "$ROOT_FOLDER/bulk_geocoder/geocode_csv.py" "$TMP_FOLDER/sales_clean.csv"
 echo 'Final processing...'
 python "$ROOT_FOLDER/bulk_geocoder/process_geocoded_csv.py" "$TMP_FOLDER/sales_geocoded.csv" "$TMP_FOLDER/sales_db.csv"
 
-# #Generate CREATE TABLE statement
-# csvsql -i postgresql --tables sales --db-schema public -d ',' "$TMP_FOLDER/sales_db.csv" > "$TMP_FOLDER/sales.sql"
-# #Drop table if exists
-# psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS sales;"  
-# #Create table
-# psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$TMP_FOLDER/sales.sql"  
+#Generate CREATE TABLE statement
+csvsql -i postgresql --tables sales --db-schema public -d ',' "$TMP_FOLDER/sales_db.csv" > "$TMP_FOLDER/sales.sql"
+#Drop table if exists
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP TABLE IF EXISTS sales;"  
+#Create table
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$TMP_FOLDER/sales.sql"  
 
-# #Upload data to the database
-# cat "$TMP_FOLDER/sales_db.csv" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "\COPY public.sales FROM STDIN  WITH CSV HEADER DELIMITER ',';"
+#Upload data to the database
+cat "$TMP_FOLDER/sales_db.csv" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "\COPY public.sales FROM STDIN  WITH CSV HEADER DELIMITER ',';"
 
-# echo 'Done creating sales table!'
+echo 'Done.'
