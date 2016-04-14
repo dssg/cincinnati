3837f2faac2e6e791a741dded9d65460da015a4a
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-04-14 12:04:37 -0500

    updates CREATE TABLE parcel2address script to attempt to create the table when already exists

--- bulk_geocoder/geocode.sh
+++ bulk_geocoder/geocode.sh
@@ -10,6 +10,11 @@ DB_NAME=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.database)
 #Create address table if it doesn't exist, also create indexes
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$BULK_GEOCODER_FOLDER/address.sql"
 
+#Creates parcel2address table, which will store records for each parcel to its
+#addresses nearby. This script only creates the table and does not perform any
+#computation
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$BULK_GEOCODER_FOLDER/parcel_to_address.sql"
+
 #Join addresses in the different datasets and filter them
 #to get the unique ones. If you incorporate a new dataset
 #with addresses, you must add it manually to get_unique_addresses.py
@@ -19,22 +24,21 @@ python "$ROOT_FOLDER/bulk_geocoder/get_unique_addresses.py"
 #in the database
 python "$ROOT_FOLDER/bulk_geocoder/update.py"
 
-#Upload data to the database
+#Upload new addresses to the database
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "\COPY address(address, zip, latitude, longitude) FROM '$ETL_FOLDER/diff_unique_addresses.csv' WITH CSV HEADER DELIMITER ',';"
 
 #This script creates a geometric point in each address with latitude and longitude
 #with a NULL value in geom column
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$BULK_GEOCODER_FOLDER/update_geoms.sql"
 
+
+###TODO: FIX CODE BELOW. IT WONT WORK WHEN UPDATING
+
 #Using the address table (which contains unique addresses),
 #add a new column in fire, crime and sales with the corresponding address_id
 echo 'Mapping addresses in addreess table with events in fire, crime and sales'
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$BULK_GEOCODER_FOLDER/match_events_with_address.sql"
 
-#Creates parcel2address table, which will store records for each parcel to its
-#addresses nearby. This script only creates the table and does not perform any
-#computation
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$BULK_GEOCODER_FOLDER/parcel_to_address.sql"
 
 #Creates already_computed_addresses table which keeps track of addresses
 #that were already used for calculating distances to parcels
--- bulk_geocoder/parcel_to_address.sql
+++ bulk_geocoder/parcel_to_address.sql
@@ -1,9 +1,9 @@
 --Table used to relate parcels with close addresses
-CREATE TABLE parcel2address (
+CREATE TABLE IF NOT EXISTS parcel2address (
     parcel_id varchar(30) NOT NULL,
     address_id integer REFERENCES address,
     dist_m real NOT NULL
 );
 
-CREATE INDEX ON parcel2address (parcel_id);
-CREATE INDEX ON parcel2address (address_id);
+CREATE INDEX parcel2address_parcel_id_index ON parcel2address (parcel_id);
+CREATE INDEX parcel2address_address_id_index ON parcel2address (address_id);
