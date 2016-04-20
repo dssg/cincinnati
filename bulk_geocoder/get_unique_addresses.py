c089b9ba601ca2ed7b2ed129d3f306e28eef117a
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-04-20 13:08:00 -0500

    finished new permits etl and adds permits dataset to geocoding etl

--- bulk_geocoder/get_unique_addresses.py
+++ bulk_geocoder/get_unique_addresses.py
@@ -5,7 +5,7 @@ path_to_etl = os.path.join(os.environ['DATA_FOLDER'], 'etl')
 path_to_fire = os.path.join(path_to_etl, 'fire', 'tmp', 'fire_db.csv')
 path_to_crime = os.path.join(path_to_etl, 'crime', 'tmp', 'crime_db.csv')
 path_to_sales = os.path.join(path_to_etl, 'sales', 'tmp', 'diff_sales_db.csv')
-path_to_sales = os.path.join(path_to_etl, 'permits', 'tmp', 'diff_permits_db.csv')
+path_to_permits = os.path.join(path_to_etl, 'permits', 'tmp', 'diff_permits_db.csv')
 
 def process_df(df):
     print '    Dataset has {:,d} rows and {:,d} columns'.format(*df.shape)
@@ -18,7 +18,11 @@ def process_df(df):
     print '    Dataset has {:,d} unique addresses'.format(len(df.index))
     return df
 
-print 'Loading fire, crime and sales. If you have more datasets with address add them here...'
+print 'Loading permits, fire, crime and sales. If you have more datasets with address add them here...'
+
+print 'Processing permits:'
+permits = pd.read_csv(path_to_permits)
+permits = process_df(permits)
 
 print 'Processing fire:'
 fire = pd.read_csv(path_to_fire)
@@ -33,7 +37,7 @@ sales = pd.read_csv(path_to_sales)
 sales = process_df(sales)
 
 print 'Merging'
-df = pd.concat([fire, crime, sales])
+df = pd.concat([permits, fire, crime, sales])
 print '    Merged dataset has {:,d} addresses'.format(len(df.index))
 df = df.drop_duplicates(subset='address')
 print '    Merged dataset has {:,d} unique addresses'.format(len(df.index))
