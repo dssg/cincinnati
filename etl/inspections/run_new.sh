6f3a2c85534a7f4d31755ba6a6cd9fa3af661fbe
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-04-19 20:59:09 -0500

    Changes to new inspections etl

--- etl/inspections/run_new.sh
+++ etl/inspections/run_new.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 LOCAL_DATA_FOLDER="$DATA_FOLDER/etl/inspections"
 LOCAL_CODE_FOLDER="$ROOT_FOLDER/etl/inspections"
+TMP_FOLDER="$LOCAL_DATA_FOLDER/tmp"
 
 #Read variables from config file
 DB_HOST=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.host)
 DB_USER=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.user)
 DB_NAME=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.database)
 
+#Oracle DB auth
+ORACLE_USER=system
+ORACLE_PWD=oracle
 
 #Build image from Dockerfile
 docker build -t ora2pg .
@@ -14,14 +18,39 @@ echo 'Docker image built'
 
 #Run container
 docker run -d -p 49160:22 -p 49161:1521 -v $DATA_FOLDER/etl/inspections:/root/data ora2pg
-
 #SSH to Docker
 #password: admin
 ssh root@localhost -p 49160
 
-#Step 1: Export Oracle dump to SQL
+#Part 1: Export Oracle dump to SQL
+#Create schema to upload dmp file
+sqlplus -s $ORACLE_USER/$ORACLE_PWD@localhost <<< "create user inspections_raw identified by password default tablespace users;"
+sqlplus -s $ORACLE_USER/$ORACLE_PWD@localhost <<< "grant connect to inspections_raw;"
+sqlplus -s $ORACLE_USER/$ORACLE_PWD@localhost <<< "grant create table to inspections_raw;"
+sqlplus -s $ORACLE_USER/$ORACLE_PWD@localhost <<< "grant unlimited tablespace to inspections_raw;"
+#Import dmp file to Oracle database
+imp SYSTEM/$ORACLE_PWD file=/root/data/inspections.dmp fromuser=DBADLS touser=inspections_raw
+#Export data to PostgreSQL
+ora2pg -t TABLE -n inspections_raw -o /root/data/tmp/inspections.tables.sql
+nohup ora2pg -t COPY -n inspections_raw -o /root/data/tmp/inspections.data.sql
 
-#Step 2: Upload data
+#Kill container
 
+#Part 2: Upload inspections data
+#Add SET SCHEMA LINE to SQL files
+sed -i -e "1iSET SCHEMA 'inspections_raw';\\" "$TMP_FOLDER/inspections.tables.sql"
+sed -i -e "1iSET SCHEMA 'inspections_raw';\\" "$TMP_FOLDER/inspections.data.sql"
+#Drop everything in case it already exists
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP VIEW  IF EXISTS inspections_views.events_and_lat_long;"
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP VIEW  IF EXISTS inspections_views.events_parcel_id;"
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP VIEW  IF EXISTS inspections_views.events;"
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP VIEW  IF EXISTS inspections_views.number_key2parcel_no;"
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$LOCAL_CODE_FOLDER/drop_inspections_tables.sql"
+#Upload data
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$TMP_FOLDER/inspections.tables.sql"
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$TMP_FOLDER/inspections.data.sql"
 
-#Step 3: Create view
+#Part 3: Create inspections views
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME < "$ROOT_FOLDER/etl/inspections/events_timeline.sql"
+
+echo 'Done uploading inspections data to postgres!'
