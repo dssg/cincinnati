935d5a03e7d963d735a9e550a9505fdecb493706
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-04-19 19:43:40 -0500

    [incomplete] new etl for inspections

--- etl/inspections/run_new.sh
+++ etl/inspections/run_new.sh
@@ -8,6 +8,17 @@ DB_USER=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.user)
 DB_NAME=$(cat $ROOT_FOLDER'/config.yaml' | shyaml get-value db.database)
 
 
+#Build image from Dockerfile
+docker build -t ora2pg .
+echo 'Docker image built'
+
+#Run container
+docker run -d -p 49160:22 -p 49161:1521 -v $DATA_FOLDER/etl/inspections:/root/data ora2pg
+
+#SSH to Docker
+#password: admin
+ssh root@localhost -p 49160
+
 #Step 1: Export Oracle dump to SQL
 
 #Step 2: Upload data
