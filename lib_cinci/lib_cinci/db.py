ce012c85ff85a8496fd13840d7ae8b89b7cc23eb
OpsWorks user bkuester <bkt@uchicago.edu>
2016-11-04 21:09:39 +0000

    minor bugfix

--- lib_cinci/lib_cinci/db.py
+++ lib_cinci/lib_cinci/db.py
@@ -3,4 +3,6 @@ from config import load
 # Returns a string contaning a URI for the given database type
 # assumes there is a config.yaml file in $ROOT folder with a db
 # key contaning host, user, password and database
-uri = '{dialect}://{user}:{password}@{host}:5432/{database}'.format(**load('config.yaml')['db'])
+connparams = load('config.yaml')['db']
+uri = '{dialect}://{user}:{password}@{host}:{port}/{database}'.format(**connparams)
+libpq_uri = 'dbname={database} user={user} host={host} password={password} port={port}'.format(**connparams)
