3760d600adf41ea7fec527dac2ef404a393775da
OpsWorks user bkuester <bkt@uchicago.edu>
2016-12-05 20:08:51 +0000

    reduce size of big grid

--- model/grid_values.py
+++ model/grid_values.py
@@ -9,7 +9,7 @@ big = {
 
     "sklearn.ensemble.AdaBoostClassifier": {
         'algorithm': ['SAMME', 'SAMME.R'],
-        'n_estimators': [1,10,100,1000,10000]
+        'n_estimators': [1,10,100,1000]
     },
     
     "sklearn.linear_model.LogisticRegression": {
@@ -23,7 +23,7 @@ big = {
     },
 
     "sklearn.ensemble.ExtraTreesClassifier": {
-        'n_estimators': [1,10,100,1000,10000],
+        'n_estimators': [1,10,100,1000],
         'criterion' : ['gini', 'entropy'],
         'max_depth': [1,5,10,20,50,100],
         'max_features': ['sqrt','log2'],
@@ -31,7 +31,7 @@ big = {
     },
 
     "sklearn.ensemble.GradientBoostingClassifier": {
-        'n_estimators': [1,10,100,1000,10000],
+        'n_estimators': [1,10,100,1000],
         'learning_rate' : [0.001,0.01,0.05,0.1,0.5],
         'subsample' : [0.1,0.5,1.0],
         'max_depth': [1,3,5,10,20,50,100]
