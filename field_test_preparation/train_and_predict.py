8d5e332e527a44054d12b19f5e3f882b6f6bd88a
OpsWorks user bkuester <bkt@uchicago.edu>
2016-12-30 21:38:26 +0000

    clean up and document train_and_predict

--- field_test_preparation/train_and_predict.py
+++ field_test_preparation/train_and_predict.py
@@ -1,9 +1,8 @@
 """
-Given a model_id that has performed well historically, train a model with 
-this model_id's configuration and parameters, but using all data up to a 
-given train_end date. Then, use this model to predict the outcome for all
-datapoints in a given prediction_schema. Return the trained model and the
-predictions.
+Given a model_id, train a model with this model_id's configuration and 
+parameters, but using all data up to a different end-of-training ('fake_today')
+date. Then, use this model to predict the outcome for all datapoints in a given 
+prediction_schema. Return the trained model and the predictions.
 """
 
 from sklearn_evaluation.Logger import Logger
@@ -14,49 +13,85 @@ from lib_cinci.dataset import get_features_for_inspections_in_schema
 import datetime
 from sklearn import preprocessing
 from pydoc import locate
+import os
+import argparse
+import logging
+import logging.config
+from lib_cinci.config import load
+
+logging.config.dictConfig(load('logger_config.yaml'))
+logger = logging.getLogger()
 
 def get_config(model_id):
     """ Give a model_id (string or ObjectId), return the entire MongoDB log
-        for that model.
+        gor that model.
     """
 
     logger = Logger(host=cfg['logger']['uri'],
                     db=cfg['logger']['db'],
                     collection=cfg['logger']['collection'])
 
+    logging.info("Fetched model_id %s."%str(model_id))
+
     return logger.get_doc_from_id(model_id)
 
 
 def train_on_config(myconfig, n_jobs=-1):
-
+    """
+    Using a configuration dictionary (like the ones that are stored in 
+    the MongoDB), train a model with the corresponding selection of data, 
+    features, parameters, etc.
+    Args:
+        myconfig (dict): A configuration dictionary, as it would be returned
+                         from the MongoDB.
+        n_jobs (int): Gets passed to scikit-learn and limits the number of 
+                      cores that it runs on.
+    Returns:
+        model (sklearn model): The fitted sklearn model.
+        imputer (sklearn imputer): The fitted sklearn imputer.
+        scaler (sklearn scaler): The fitted sklearn scaler.
+        features (list): The parsed list of features from the config file.
+    """
+    
+    # parse the configuration dictionary 
     features = parse_feature_pattern_list(myconfig["config"]["features"])
     start_date = datetime.datetime.strptime(myconfig["config"]["start_date"], '%d%b%Y')
     fake_today = datetime.datetime.strptime(myconfig["config"]['fake_today'], '%d%b%Y')
     only_residential = myconfig["config"]["residential_only"]
 
+    logging.info("Length of feature list: %s"%str(len(features)))
+    logging.info("Fetching training data from %s to %s."%(str(start_date),
+                                                          str(fake_today)))
+
+    # fetch the training data, using the dates from the config dict
     train = dataset.get_training_dataset(
         features=features,
         start_date=start_date,
         end_date=fake_today,
         only_residential=only_residential)
 
+    logging.info("Size of training dataset: %s"%str(train.x.shape))
+
+    # fit the imputer and scaler
     imputer = preprocessing.Imputer().fit(train.x)
     train.x = imputer.transform(train.x)
-
     scaler = preprocessing.StandardScaler().fit(train.x)
     train.x = scaler.transform(train.x)
 
     # Here comes a hack: in the MongoDB, we only store e.g.
-    # 'RandomForestClassifier'; but we need '
-    # sklearn.ensemble.RandomForestClassifier'. Fortunately, the longer name
-    # are stored in myconfig['config']['models']
+    # 'RandomForestClassifier'; but we need 
+    # 'sklearn.ensemble.RandomForestClassifier'. Fortunately, the longer names
+    # are stored in myconfig['config']['models'].
     modelclasses = [x for x in myconfig['config']['models']
                     if x.endswith(myconfig['name'])]
     if len(modelclasses) > 1:
         raise ValueError('Failed to uniquely identify model %s'%myconfig['name'])
 
+    # fit the model
     modelclass = modelclasses[0]
     model = locate(modelclass)(**myconfig['config']['parameters'])
+
+    logging.info("Selected algorithm: %s"%str(model))
 
     if hasattr(model, 'n_jobs'):
         model.set_params(n_jobs=n_jobs)
@@ -64,29 +99,49 @@ def train_on_config(myconfig, n_jobs=-1):
     # SVC does not predict probabilities by default
     if hasattr(model, 'probability'):
         model.probability = True
-
+    
+    logging.info("Starting sklearn fitting...")
     model.fit(train.x, train.y)
+    logging.info("Finished sklearn fitting.")
 
     return model, imputer, scaler, features
 
-def predict_on_date(prediction_schema,model,imputer,scaler,features):
 
+def predict_on_date(prediction_schema, model, imputer, scaler, features):
+    """
+    Given a fitted set of imputer, scaler, and model, and a list of features
+    that the model needs, make predictions for all parcels, using the data
+    from one of the prediction schemas (where the input features are calculated
+    up to some chosen date).
+    Args:
+        prediction_schema (str): Name of a schema in the Postgres DB, like
+                                 'features_31aug2016'. This data will be used
+                                 for making predictions.
+        model (sklearn model): Fitted sklearn model.
+        imputer (sklearn imputer): Fitted sklearn imputer.
+        scaler (sklearn scaler): Fitted sklearn scaler.
+        features (list): List of features for the above model.
+    Returns (pd.DataFrame):
+        DataFrame, indexed by parcel_id, that gives a prediction for each parcel.
+    """
+
+    # fetch the features from the prediction_schema
     dataset = get_features_for_inspections_in_schema(prediction_schema, features)
 
-    # Impute
+    logging.info("Size of prediction dataset: %s."%str(dataset.x.shape))
+
+    # apply the learned sklearn model
     dataset.x = imputer.transform(dataset.x)
-    # Scale
     dataset.x = scaler.transform(dataset.x)
-    # Predict
     preds_proba = model.predict_proba(dataset.x)[:, 1]
-    # Convert to Dataset to DataFrame and subselect only the viol_outcome
-    # column
+
+    # clean up the dataframe
     df = dataset.to_df()[['viol_outcome']]
     df['prediction'] = preds_proba
-
     df = df.reset_index().drop(['inspection_date','viol_outcome'],1)
 
     return df.set_index('parcel_id')
+
 
 def main(model_id, train_end_date, prediction_schema, n_jobs=-1):
     """
@@ -107,16 +162,17 @@ def main(model_id, train_end_date, prediction_schema, n_jobs=-1):
                                             '%d%b%Y')
     train_len = fake_today - start_date
 
-    print "Original start date: ", start_date
-    print "Original end date: ", fake_today
-    print "Train len: ", train_len
+    logging.info("Config start date: %s"%str(start_date))
+    logging.info("Config fake_today: %s"%str(fake_today))
+    logging.info("Training window length: %s."%str(train_len))
 
     new_fake_today = datetime.datetime.strptime(train_end_date, '%d%b%Y')
     new_start_date = new_fake_today - train_len
 
-    print "new end: ", new_fake_today
-    print "new start: ", new_start_date
-    print "new len: ", new_fake_today - new_start_date
+    logging.info("Shifted start date: %s"%str(new_start_date))
+    logging.info("Shifted fake_today: %s"%str(new_fake_today))
+    logging.info("Shifted training window length: %s."%str(
+                                new_fake_today-new_start_date))
 
     # now update the config
     myconfig['config']['start_date'] = datetime.datetime.strftime(new_start_date,
@@ -131,4 +187,34 @@ def main(model_id, train_end_date, prediction_schema, n_jobs=-1):
     return predict_on_date(prediction_schema,model,imputer,scaler,features)
 
 if __name__ == '__main__':
-    pass
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-mid", "--model_id",
+                        help=("MongoDB ID string for the model that you'd like to "
+                              "fit and use for prediction."), type=str)
+    parser.add_argument("-ed", "--train_end_date",
+                        help=("String, like '30Jun2016', which marks the end of "
+                              "the training data."), type=str)
+    parser.add_argument("-ps", "--prediction_schema",
+                        help=("String, like 'features_31aug2016', giving the name "
+                              "of the schema from which the features for prediction "
+                              "will be drawn."), type=str)
+    parser.add_argument("-n", "--n_jobs", type=int, default=-1,
+                            help=("n_jobs flag passed to scikit-learn models, "
+                                  "fails silently if the model does not support "
+                                  "such flag. Defaults to -1 (all jobs possible)"))
+    parser.add_argument("-o", "--outfile",
+                        help=("Path to the CSV file which contains "
+                              "the predictions. Defaults to "
+                              "$ROOT_FOLDER/field_test_preparation/predictions.csv"),
+                        type=str, default=os.path.join(os.environ["ROOT_FOLDER"], 
+                            "field_test_preparation","predictions.csv"))
+    args = parser.parse_args()
+
+    df = main(args.model_id, args.train_end_date, 
+            args.prediction_schema, args.n_jobs)
+
+    if args.outfile:
+        with open(args.outfile,'w') as fout:
+            df.to_csv(fout, index=True)
+
