2a355c5b548110e2211a117445df726c13458366
jzanzig <jmlzanzig@gmail.com>
2017-03-22 16:42:41 +0000

    add env_sample.sh

---
+++ model_exploration/env_sample.sh
@@ -0,0 +1,13 @@
+#Where to store the code
+export ROOT_FOLDER=
+#Where data is stored
+export DATA_FOLDER=
+#Where to output results from models
+export OUTPUT_FOLDER=
+#Add lib folder to PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$ROOT_FOLDER/lib_cinci
+
+export PGHOST=
+export PGDATABASE=
+export PGUSER=
+export PGPASSWORD=
