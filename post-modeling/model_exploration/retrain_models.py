873657e027aa81e6c032e3c28622e31d420868c3
jzanzig <jmlzanzig@gmail.com>
2017-03-27 19:21:30 +0000

    save feature importances as one CSV for all models instead of one for each

--- post-modeling/model_exploration/retrain_models.py
+++ post-modeling/model_exploration/retrain_models.py
@@ -12,27 +12,31 @@ model_groups = [18711, 1120, 14613, 5716,
 
 k = 7500 # top 5% of parcels
 
+# TODO : merge these?
 all_models = pd.read_csv('model-results-2017-03-23.csv', index_col='model_id')
+models_grouped = pd.read_csv('model-results-grouped.csv')
+
+# get model ids from model columns
+model_id_columns = [col for col in list(models_grouped) if col.startswith('model_id')]
 
 all_top_k = {}
-model_ids = ['585b70bcba2d6c88519e89fd']
+all_feature_importances = {}
+
 parcel_info = pd.read_csv('parcels-with-neighborhood-info.csv', index_col='parcel_id')
 
-#for m in model_groups: 
-for model_id in model_ids:
-#    model_group = str(m)
-    model_group = model_id
-    model_name = all_models.loc[model_id, 'name']
+#for model_id in model_ids:
+for model_group in model_groups:
+    model_id = models_grouped.loc[model_group, model_id_columns].dropna()[0]
 
     # Retrain model and get predictions on all parcels
     trained_model_df, trained_model_dict = main(model_id=model_id, 
                                                 train_end_date='30Aug2016',  
-                                                prediction_schema='features_31aug2016', # todo : make variable
+                                                prediction_schema='features_31aug2016',
                                                 return_features=True, 
                                                 return_fitted=True)       
     trained_model_df.sort_values('prediction', ascending=False, inplace=True)
 
-    # Add neighborhood metrics for each parcel and save to CSV
+    # Add neighborhood metrics for each parcel 
     model_predictions = trained_model_df[['prediction']].join(parcel_info)
     all_top_k[model_group] = model_predictions.head(7500)
 
@@ -46,11 +50,14 @@ for model_id in model_ids:
                                                    trained_model_dict['model'].feature_importances_]).T
 
     feature_importances.columns = ['feature', 'feature_importance']
+    feature_importances['model_group'] = model_group
     output_path = os.path.join(output_folder, 
                                'feature_importances', 
                                'feature_importances_' + model_group + '.csv')
     feature_importances.to_csv(output_path)
     
+    all_feature_importances[model_group] = feature_importances
+
     # Get list of top k parcels below median ID
     inspection_density_median = model_predictions['inspection_density'].median()
     median_mask = model_predictions.inspection_density < inspection_density_median
@@ -61,9 +68,12 @@ for model_id in model_ids:
     inspection_density_first_quartile = model_predictions.inspection_density.quantile(0.25)
     first_quartile_mask = model_predictions.inspection_density < inspection_density_first_quartile
     below_quartile_ID = model_predictions[first_quartile_mask].head(k)
-    all_top_k[model_group + ' Below First Quaritle ID'] = below_quartile_ID
+    all_top_k[model_group + ' Below First Quartile ID'] = below_quartile_ID
     
-all_top5 = pd.concat(all_top_k.values())
+all_top5 = pd.concat(all_top_k.values(), keys= all_top_k.keys())
 all_top5['violations_per_house'] = all_top5['violation_rate'] * all_top5['inspection_density'] 
 all_top5.to_csv('all_top5.csv')
+
+all_features_csv = pd.concat(all_feature_importances.values())
+all_features_csv.set_index(['feature','model_group']).unstack().to_csv('features_test.csv')
 
