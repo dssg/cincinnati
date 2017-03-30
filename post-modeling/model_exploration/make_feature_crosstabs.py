d6a87dc42f6cd755b5499846277fce558933fed6
jzanzig <jmlzanzig@gmail.com>
2017-03-30 18:40:57 +0000

    alter feature crosstab table to have one feature per row

--- post-modeling/model_exploration/make_feature_crosstabs.py
+++ post-modeling/model_exploration/make_feature_crosstabs.py
@@ -18,19 +18,20 @@ libpq_uri = 'dbname={database} user={user} host={host} password={password} port=
 engine = create_engine(uri)
 
 # get all tables from feature schema, excluding
-# 'insp2' tables - they are lookups, not features
+# 'insp2' tables (they are lookups, not features),
+# named_entities and parcels_inspections
 query = '''
         SELECT DISTINCT table_name 
         FROM information_schema.tables
         WHERE table_schema = 'features_31aug2016'
-        AND SUBSTRING(table_name FROM 1 FOR 5) != 'insp2';
+        AND SUBSTRING(table_name FROM 1 FOR 5) != 'insp2'
+        AND table_name NOT IN ('parc_year', 'parcels_inspections', 'named_entities');
         '''
 
 all_tables = pd.read_sql(query, engine)
 all_features = {}
 
-## FOR MODEL FEATURE AVERAGES
-for table in all_tables.table_name.values[0:2]:
+for table in all_tables.table_name.values:
     column_query = '''
               SELECT DISTINCT column_name 
               FROM information_schema.columns
@@ -40,32 +41,59 @@ for table in all_tables.table_name.values[0:2]:
               AND column_name != 'inspection_date'
               AND data_type IN ('double precision', 'bigint', 'integer');
               '''.format(table=table)
+    
     columns = pd.read_sql(column_query, engine)
+    column_names = list(columns.column_name.values)
 
     model_query = 'SELECT model_number, ' 
     all_query = 'SELECT 1 as model_number, '
-
-    for col in list(columns.column_name.values)[:-1]:
-        model_query += 'AVG({col}) AS {table}_{col}, '.format(table=table, col=col)
-        all_query += 'AVG({col}) AS {table}_{col}, '.format(table=table, col=col)
-
-    model_query += '''
-        AVG ({col}) AS {table}_{col}
-        FROM all_top_five 
-        JOIN features_31aug2016.{table} t
-        ON all_top_five.parcel_id = t.parcel_id
-        GROUP BY model_number
-        '''.format(col=columns.column_name.values[-1], table=table)
-
-    all_query += 'AVG ({col}) AS {table}_{col} FROM features_31aug2016.{table} GROUP BY model_number;'.format(col=columns.column_name.values[-1], table=table)
-        
-    average_top_five = pd.read_sql(model_query, engine, index_col = 'model_number')
-    average_top_five['table_name'] = table
-    average_top_five['group'] = 'all parcel average'
     
-    average_all_parcels = pd.read_sql(all_query, engine, index_col = 'model_number')
-    average_all_parcels['table_name'] = table
-    average_all_parcels['group'] = 'top k average' 
+    if len(column_names) > 1:    
+        for col in list(columns.column_name.values)[:-1]:
+            model_query += 'AVG("{col}") AS "{table}_{col}", '.format(table=table, col=col)
+            all_query += 'AVG("{col}") AS "{table}_{col}", '.format(table=table, col=col)
 
-    all_features[table] = pd.concat([average_all_parcels, average_top_five])
-pd.concat(all_features.values()).to_csv('feature_crosstabs_test.csv')
+        model_query += '''
+                        AVG ("{col}") AS "{table}_{col}"
+                        FROM all_top_five 
+                        JOIN features_31aug2016.{table} t
+                        ON all_top_five.parcel_id = t.parcel_id
+                        GROUP BY model_number;
+                       '''.format(col=column_names[-1], table=table)
+
+        all_query += '''
+                        AVG ("{col}") AS "{table}_{col}" 
+                        FROM features_31aug2016.{table} 
+                        GROUP BY model_number;
+                      '''.format(col=column_names[-1], table=table)
+    
+    elif len(column_names) == 1:
+         model_query = '''
+                       SELECT model_number, AVG("{col}") AS "{table}_{col}" 
+                       FROM all_top_five 
+                       JOIN features_31aug2016.{table} t
+                       ON all_top_five.parcel_id = t.parcel_id 
+                       GROUP BY model_number
+                       '''.format(col=column_names[0], table=table)
+         
+         all_query = '''
+                     SELECT 1 AS model_number, AVG("{col}") AS "{table}_{col}"
+                     FROM features_31aug2016.{table} 
+                     GROUP BY model_number;
+                     '''.format(col=column_names[0], table=table)
+
+    elif len(column_names) == 0:
+        continue
+
+    average_top_five = pd.read_sql(model_query, engine, index_col = 'model_number')
+    average_all_parcels = pd.read_sql(all_query, engine, index_col = 'model_number')
+    ratio_top_five_to_all = average_top_five.divide(average_all_parcels.loc[1], axis=1)
+
+    all_features[table] = pd.concat([average_top_five, average_all_parcels, ratio_top_five_to_all],
+                                     keys=['Model Top 5% Average', 'All Parcel Average', 'Ratio'],
+                                     names=['value'])
+
+all_crosstabs = pd.concat(all_features.values(), axis=1)
+all_crosstabs.reset_index(inplace=True)
+all_crosstabs.sort_values('model_number').T.to_csv('feature_crosstabs.csv', header=False)
+
