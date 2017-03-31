1683f75e2c75cf838bccd25f3be8092b17dfc713
jzanzig <jmlzanzig@gmail.com>
2017-03-31 16:27:17 +0000

    get model groups for list overlap from top model list rather than hard coding

--- post-modeling/model_exploration/make_list_overlap_heatmap.py
+++ post-modeling/model_exploration/make_list_overlap_heatmap.py
@@ -12,34 +12,14 @@ from sklearn_evaluation.plots import compute_similarity
 
 all_top5 = pd.read_csv('all_top5.csv', index_col=0)
 top_models = pd.read_csv('top_model_reason_lookup.csv')
-model_groups = top_models.model_group.values
+model_groups = top_models.model_group.map(str)
 
 top_k = {}
 for model_group in model_groups:
-    model_group = str(model_group)
     top_k[model_group] = all_top5.loc[model_group]
 
-top_k_keys = {'10062': 'ID (10062)', 
-              '12547': 'ID (12547)',  
-             '7111': 'ID (7111)',
-             '28108': 'ID (28108)', 
-             '11814': 'ID (11814)',
-              '18711': 'P@5 (18711)',
-              '1120': 'P@5 (1120)',
-              '14613': 'P@5 (14613)',
-              '5716': 'P@5 (5716)',
-              '7520': 'All 3 (7520)',
-              '1309': 'All 3 (1309)',
-              '26523': 'All 3 (26523)',
-              '28879': 'All 3 (28879)',
-              '27039': 'VR (27039)',
-              '7068': 'VR (7068)',
-              '29230': 'VR (29230)',
-              '25683': 'VR (25683)'
-             }
-
 d = {k: v.parcel_id.values for k,v in top_k.iteritems()}
-top_k_dict = {top_k_keys[k]: v for k, v in d.items()}
+top_k_dict = {top_k_labels[k]: v for k, v in d.items()}
 prediction_matrix = pd.DataFrame.from_dict(top_k_dict)
 df = compute_similarity(prediction_matrix)
 cmap = sns.cubehelix_palette(dark=0, light=1, start=.5, rot=-.75, as_cmap=True)
