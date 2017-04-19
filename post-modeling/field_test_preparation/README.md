07291a052c8d72ae3a2f67aa60f12b74f0e946fd
jzanzig <jmlzanzig@gmail.com>
2017-04-19 13:54:30 +0000

    Move list generation and drake steps to field test preparation

--- post-modeling/field_test_preparation/README.md
+++ post-modeling/field_test_preparation/README.md
@@ -1,10 +1,50 @@
-# Preparing Results for a Field Test [WORK IN PROGRESS]
+# Preparing Results for a Field Test - Retraining Models & Generating Lists
+First, you should have chosen a handful of models to retrain (see model exploration 
+section).
 
-This folder contains tools to create a list of inspections using a previously trained model.
+Based on the results of the [model exploration step](../model_exploration/), 
+we did some hands-on analysis (primarily using Tableau) to select 5 models 
+that did the best with respect to precision (finding the 
+most violations), 5 models that identified parcels in neighborhoods that had a low
+inspection density, 5 models that identified parcels that had a low
+neighborhood violation rate, and 5 models that seemed to balance these three 
+aspects fairly well. Update the CSV here with model group numbers with their respective 
+reasons for selection in a table ([`top_model_reason_lookup.csv`](top_model_reason_lookup.csv))
+to be used for the following steps.
 
----
+## Retrain Models & Make a List
+Once you've chosen your final model(s), you'll want to retrain them using 
+the most recent data available (in our case, the most recent data update was August 31,
+2016). The [`retrain_models.py`](retrain_models.py) script does this, creating
+a list of the top *k* parcels for each model, as well as a list of the 
+model-specific feature importances.
 
-Summer folders:
+To create a list of parcels to inspect, we use [`generate_list.py`](generate_list.py). 
+Given a model group number, this will generate a list of parcels to inspect, 
+along with address, latitude, longitude, and number of inspections and violations in 
+the neighborhood of that parcel.
 
-* [postprocess](postprocess/) - Add details (e.g. address) about properties to predictions
-* [targeting_priority](targeting_priority/) - Re-rank predictions according to some targeting priority
+Once you have chosen a model and which list subset you want (options: `'All Parcels'`, 
+`'Below Insp. Density First Quartile'`, or `'Below Insp. Density Median'`), run
+`generate_list.py` to create a list (`inspection_list.csv`). 
+`python generate_list.py '23049' 'All Parcels'` 
+
+## Feature Crosstabs: **_Which types of parcels are ranked highly by each model?_**
+We'd like to analyze how the models are making decisions, but sometimes 
+interpreting ML models isn't totally straightforward, and sometimes it's 
+hard to know if you're comparing "apples to apples." One way we can analyze models
+and interpret the mechanisms by which they make decisions is by characterizing the 
+"riskiest" parcels a given model identifies and comparing how the data we have about
+these risky parcels differs from what we know about the general population of parcels. We do
+this by gathering all of the information we have (all features) for all 
+parcels, taking the average across all parcels, and then comparing that to
+the average of those features across the top-ranked parcels for each model.  
+
+To make the feature crosstabs, run the [`make_feature_crosstabs.py`](make_feature_crosstabs.py)
+script. 
+
+## List Overlap Between Models: **_Does it really matter which model I choose?_**
+It's possible that you'd end up with the same list of parcels to inspect, 
+regardless of the model. To investigate this, we use [`make_list_overlap_heatmap.py`](make_list_overlap_heatmap.py)
+to create a heatmap displaying *overlap* between the top `k` parcels chosen 
+by each model. Example [here](list_overlap_heatmap.png).
--- post-modeling/field_test_preparation/generate_list.py
+++ post-modeling/field_test_preparation/generate_list.py
@@ -1,107 +1,35 @@
-from datetime import datetime
-
-from sklearn_evaluation.Logger import Logger
 import pandas as pd
 from sqlalchemy import create_engine
+import os
+import sys
+import yaml
+from lib_cinci.config import load
 
-from lib_cinci.config import main
-from lib_cinci.predict_on_schema import predict_on_schema
-from lib_cinci.evaluation import add_inspections_results_for
-from lib_cinci.db import uri
+folder = os.environ['ROOT_FOLDER']
+name = 'config.yaml'
+path = "%s/%s" % (folder, name)
+f = open(path, 'r')
+text = f.read()
+main = yaml.load(text)
 
-# generate a list of inspections based on a model predictions
+connparams = load('config.yaml')['db']
+uri = '{dialect}://{user}:{password}@{host}:{port}/{database}'.format(**connparams)
+libpq_uri = 'dbname={database} user={user} host={host} password={password} port={port}'.format(**\
+connparams)
 
-# parameters
-
-# experiment name
-exp_name = 'exp4_spr16_jan13_may15_only_400m'
-# metric to select the best trial for a given experiment
-metric = 'prec_at_5'
-# db schema used to get the features from
-schema = 'features_01may2015'
-# top_percent - percent taken from the top
-# used for random sampling
-percent = 4
-# n_inspections - list length
-n_inspections = 100
-# nei_schema - db schema used to look for the neighborhood score
-# table
-nei_schema = 'features_01may2015'
-# nei_table - db table used to get the neighborhood scores from
-nei_table = 'neighborhood_score_500m_6months'
-
-# step 1 - load model
-
-# load information for the best model
-logger = Logger(host=main['logger']['uri'],
-                db=main['logger']['db'],
-                collection=main['logger']['collection'])
-model = logger.get_best_from_experiment(exp_name, metric)
-
-# training window
-train_start = datetime.strptime(model['config']['start_date'], '%d%b%Y')
-train_end = datetime.strptime(model['config']['fake_today'], '%d%b%Y')
-# change the format to match the one needed for the db
-train_start = train_start.strftime('%Y-%m-%d')
-train_end = train_end.strftime('%Y-%m-%d')
-today = datetime.utcnow().strftime('%Y-%m-%d')
-
-# step 2 - rank all parcels in cincinnati
-
-# use model to predict on every parcel in a given list
-preds = predict_on_schema(str(model['_id']), schema)
-
-# drop inspection date column and viol_outcime, we don't need it
-preds.reset_index(inplace=True)
-preds.drop('inspection_date', axis=1, inplace=True)
-preds.drop('viol_outcome', axis=1, inplace=True)
-preds.set_index('parcel_id', inplace=True)
-# add rank column
-preds['score_rank'] = preds.prediction.rank(method='min', ascending=False)
-
-total = preds.shape[0]
-# sort by score
-preds.sort_values(by='prediction', inplace=True, ascending=False)
-
-# step 3 - add extra information
-# add inspection results from the last date
-# used for traininig until now
-preds = add_inspections_results_for(preds, train_end, today)
-
-# add type of parcel
-
-# add neighborhood score
 engine = create_engine(uri)
-nei_score = pd.read_sql_table(nei_table, engine, schema,
-                              index_col=['parcel_id', 'inspection_date'])
-nei_score['metric'] = (nei_score.unique_violations / nei_score.houses)
-nei_score['insp_density'] = (nei_score.unique_inspections / nei_score.houses)
-nei_score.sort_values(by='metric', ascending=False, inplace=True)
-# remove unused columns
-nei_score.reset_index(inplace=True)
-nei_score.drop('inspection_date', axis=1, inplace=True)
-nei_score.set_index('parcel_id', inplace=True)
-# Filter neighborhood score table...
-# since not all cincinnati parcels have been inspected we cannot
-# truste the neighborhood score in areas that don't have inspections
-insp_density_median = nei_score.insp_density.median()
-sub_score = nei_score[nei_score.insp_density >= insp_density_median]
-preds = preds.join(nei_score[['metric']])
 
-# step 4 - filter
-# filter by parcel type? (or maybe group them)
-# filter by number of inspections since X
-# filter using neighborhood score?
+model_group = sys.argv[1]
+subset = sys.argv[2]
 
+query = '''
+        SELECT * 
+        FROM model_results.all_top_k top_k
+        LEFT JOIN model_results.parcel_info
+        USING (parcel_id)
+        WHERE top_k.model_group = '{model_group}'
+        AND subset = '{subset}'
+        '''.format(model_group=model_group, subset=subset)
 
-# step 5 - take the top predictions
-top_k = int(total*(4/100.0))
-subset = preds.head(top_k)
-
-# subset[subset.inspections.notnull()]
-
-# step 6 - random samplong from the top predictions
-inspections_list = subset.sample(n_inspections)
-
-# step 7 - subset columns and save to csv
-inspections_list[['prediction']].to_csv('list-june16.csv')
+model_list = pd.read_sql(query, engine, index_col='parcel_id')
+model_list.to_csv('inspection_list.csv')
