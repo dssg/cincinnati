568b5f0ee46fd7cbf76fb07a7ba71e1e8cc95ae8
jzanzig <jmlzanzig@gmail.com>
2017-04-19 14:03:18 +0000

    - Remove Postgres connections from get_all_model_info.py

--- post-modeling/model_exploration/get_all_model_info.py
+++ post-modeling/model_exploration/get_all_model_info.py
@@ -45,9 +45,8 @@ main = yaml.load(text)
 
 connparams = load('config.yaml')['db']
 uri = '{dialect}://{user}:{password}@{host}:{port}/{database}'.format(**connparams)
-libpq_uri = 'dbname={database} user={user} host={host} password={password} port={port}'.format(**connparams)
+#libpq_uri = 'dbname={database} user={user} host={host} password={password} port={port}'.format(**connparams)
 
-engine = create_engine(uri)
 logger = Logger(host=main['logger']['uri'], db=main['logger']['db'], 
                 collection=main['logger']['collection'])
 
@@ -69,19 +68,6 @@ all_models = [item for sublist in all_models_by_experiment for item in sublist]
 
 for m in all_models:
     m['model_id'] = str(m['_id'])
-
-#get neighborhood feature table 
-#neighborhood_table = 'neighborhood_score_' + space_delta + '_' + time_delta
-#neighborhood_history = pd.read_sql_table(neighborhood_table, engine,
-#                                         schema = validation_feature_schema,
-#                                         index_col = 'parcel_id')
-#neighborhood_history.drop('inspection_date', axis=1, inplace=True)
-
-#neighborhood_history['violations_per_house'] = neighborhood_history.unique_violations/neighborhood_history.houses
-#violations_per_house_first_quartile = neighborhood_history['violations_per_house'].quantile(0.25)
-
-#neighborhood_history['inspection_density'] = neighborhood_history.unique_inspections/neighborhood_history.houses
-#inspection_density_first_quartile = neighborhood_history['inspection_density'].quantile(0.25)
 
 for model in all_models:
     # get validation start date, end date 
@@ -110,19 +96,6 @@ for model in all_models:
                                                        percent=1.0, # precision is calculated on top k, so use 100%
                                                        ignore_nas=True)[0] # not all parcels in top k will have inspections
     model['validation_labeled_percent'] = 100.0*(top_k_inspected.viol_outcome.notnull().sum())/k
-
-    # add neighborhood info - mean and std dev of inspection density and violations per house 
-    # on top k parcels for this model
-
-#    top_k_neighborhood = model_top_k.join(neighborhood_history)
-
- #   model['top_5_inspection_density_mean'] = top_k_neighborhood['inspection_density'].mean()
- #   model['top_5_inspection_density_std_dev'] = top_k_neighborhood['inspection_density'].std()
- #   model['top_5_low_inspection_density_percent'] = (top_k_neighborhood['inspection_density'] < inspection_density_first_quartile).mean()
-
-  #  model['top_5_violations_per_house_mean'] = top_k_neighborhood['violations_per_house'].mean()
-  #  model['top_5_violations_per_house_std_dev'] = top_k_neighborhood['violations_per_house'].std()
-  #  model['top_5_low_violations_per_house_percent'] = (top_k_neighborhood['violations_per_house'] < violations_per_house_first_quartile).mean()
 
 all_models_df = pd.DataFrame(all_models)
 all_models_df.set_index('model_id', inplace=True)
